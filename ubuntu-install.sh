0294bc9f60893a1329e93740011f5ec76edf2c64
gh-liu <liugh.cs@gmail.com>
2021-11-11 07:23:26 +0000

    add protobuf

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -10,7 +10,7 @@ sudo apt install -y gdb binutils
 sudo apt install -y linux-tools-$(uname -r) linux-tools-generic
 sudo apt install -y autoconf automake pkg-config
 sudo apt install -y ripgrep silversearcher-ag
-sudo apt install -y direnv hugo
+sudo apt install -y direnv hugo protobuf-compiler
 
 ## directories
 tools=~/tools
@@ -102,3 +102,7 @@ sudo make install
 
 source ~/.zshrc
 vim +PlugInstall +qall
+
+## protobuf go
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
