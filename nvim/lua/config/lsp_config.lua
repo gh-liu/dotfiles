41b88239f62192f877b7adf3d054bea5975ff4ff
gh-liu <liugh.cs@gmail.com>
2021-11-29 23:28:40 +0000

    nvim: update

--- nvim/lua/config/lsp_config.lua
+++ nvim/lua/config/lsp_config.lua
@@ -8,34 +8,55 @@ local lsp = require "lspconfig"
 -- lsp settings
 vim.lsp.set_log_level("debug")
 
--- lsp keybingdings
-local opts = {
-    noremap = true,
-    silent = true
-}
--- go-to-definition
--- map('n','<c-]>','<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-map('n', '<c-d>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-map('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-map('n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
--- find-references
-map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
--- hover
-map('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-map('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
--- completion
--- rename
-map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
--- format
--- refactor
-map('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
--- diagnostic
-map('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-map('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
--- something else
-map('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
-map('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<cr>', opts)
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+    end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = {
+        noremap = true,
+        silent = true
+    }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- go-to-definition
+    -- buf_set_keymap('n','<c-]>','<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    buf_set_keymap('n', '<c-d>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    -- find-references
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    -- hover
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    buf_set_keymap('n', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    -- completion
+    -- rename
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    -- format
+    -- buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    -- refactor
+    buf_set_keymap('n', '<leader>a', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    -- diagnostic
+    -- buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    -- something else
+    buf_set_keymap('n', 'g0', '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
+    buf_set_keymap('n', 'gW', '<cmd>lua vim.lsp.buf.workspace_symbol()<cr>', opts)
+    -- buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    -- buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    -- buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+
+end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -55,7 +76,8 @@ lsp.gopls.setup {
     init_options = {
         usePlaceholders = true
     },
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 
 function goimports(timeout_ms)
@@ -98,9 +120,12 @@ end
 
 autocmd('goimports', {[[BufWritePre *.go lua goimports(1000)]]}, true)
 
+lsp.golangci_lint_ls.setup {}
+
 -- Bash
 lsp.bashls.setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 
 -- Lua
@@ -135,6 +160,18 @@ lsp.sumneko_lua.setup {
             }
         }
     },
-    capabilities = capabilities
+    capabilities = capabilities,
+    on_attach = on_attach
 }
 
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+-- for _, lsp_name in ipairs(servers) do
+--     lsp[lsp_name].setup {
+--         on_attach = on_attach,
+--         capabilities = capabilities,
+--         flags = {
+--             debounce_text_changes = 150
+--         }
+--     }
+-- end
