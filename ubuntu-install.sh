b95d01dc1d5045a66c55e44323b14623549a8d60
gh-liu <liugh.cs@gmail.com>
2021-11-29 07:19:19 +0000

    install: ubuntu update

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -2,16 +2,15 @@
 
 ## basic tools
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y vim zsh tmux tmuxp
 sudo apt install -y git ssh
-sudo apt install -y python python3-pip
-sudo apt install -y graphviz hugo
-sudo apt install -y gdb binutils
+sudo apt install -y vim zsh tmux tmuxp
+sudo apt install -y autoconf automake pkg-config 
 sudo apt install -y linux-tools-$(uname -r) linux-tools-generic
-sudo apt install -y autoconf automake pkg-config
+sudo apt install -y gdb binutils cgroup-tools
 sudo apt install -y ripgrep silversearcher-ag fd-find
-sudo apt install -y direnv hugo protobuf-compiler apache2-utils tldr ngrep
-sudo apt install -y cgroup-tools
+sudo apt install -y apache2-utils ngrep
+sudo apt install -y python python3-pip
+sudo apt install -y hugo tldr direnv graphviz protobuf-compiler
 
 ### install docker by this (https://docs.docker.com/engine/install/ubuntu/)
 
