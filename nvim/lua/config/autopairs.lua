5ff673d4bb229896117105bb4a48014da3c22904
gh-liu <liugh.cs@gmail.com>
2021-12-13 16:59:40 +0800

    nvim: update

--- nvim/lua/config/autopairs.lua
+++ nvim/lua/config/autopairs.lua
@@ -1,2 +1,9 @@
-local ap = require("nvim-autopairs")
-ap.setup()
+local npairs = require('nvim-autopairs')
+
+npairs.setup({
+    check_ts = true
+})
+
+local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
+local ts_conds = require('nvim-autopairs.ts-conds')
