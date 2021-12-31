6263182cdb03e82a2e97205646b23ab9cb86758e
gh-liu <liugh.cs@gmail.com>
2021-12-31 10:53:03 +0800

    nvim: .

--- nvim/lua/packer_set.lua
+++ nvim/lua/packer_set.lua
@@ -14,8 +14,18 @@ end
 
 -- Packer Commands
 vim.cmd([[command! WhatHighlight :call util#syntax_stack()]])
-vim.cmd([[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]])
-vim.cmd([[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]])
-vim.cmd([[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]])
-vim.cmd([[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]])
-vim.cmd([[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]])
+vim.cmd(
+  [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
+)
+vim.cmd(
+  [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
+)
+vim.cmd(
+  [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
+)
+vim.cmd(
+  [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
+)
+vim.cmd(
+  [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+)
