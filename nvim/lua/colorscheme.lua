8d0f54bd52420d1ddefa3f08ccd4b131a77ca777
gh-liu <liugh.cs@gmail.com>
2022-02-09 15:47:49 +0800

    nvim: .

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -2,4 +2,9 @@ local opt = require("utils").opt
 
 opt("background", "dark")
 vim.cmd([[colorscheme gruvbox-material]])
+--  Set contrast.
+--  This configuration option should be placed before `colorscheme gruvbox-material`.
+--  Available values: 'hard', 'medium'(default), 'soft'
+vim.g.gruvbox_material_background = "hard"
+
 -- vim.cmd([[colorscheme codedark]])
