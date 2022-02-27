e1d4fffce11a986f8ef5a21b4041add48bb06121
gh-liu <liugh.cs@gmail.com>
2022-02-27 11:43:56 +0800

    nvim: .

--- nvim/lua/modules/plugins/luasnip.lua
+++ nvim/lua/modules/plugins/luasnip.lua
@@ -20,7 +20,7 @@ local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
 ls.config.set_config({
-  history = false,
+  history = true,
   updateevents = "TextChanged,TextChangedI",
   delete_check_events = "TextChanged",
   ext_opts = {
@@ -44,6 +44,18 @@ ls.config.set_config({
   ext_base_prio = 300,
   ext_prio_increase = 1,
   enable_autosnippets = false,
+  -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+  -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+  store_selection_keys = "<Tab>",
+  -- luasnip uses this function to get the currently active filetype. This
+  -- is the (rather uninteresting) default, but it's possible to use
+  -- eg. treesitter for getting the current filetype by setting ft_func to
+  -- require("luasnip.extras.filetype_functions").from_cursor (requires
+  -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+  -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+  ft_func = function()
+    return vim.split(vim.bo.filetype, ".", true)
+  end,
 })
 
 -- args is a table, where 1 is the text in Placeholder 1, 2 the text in
@@ -57,101 +69,12 @@ local rec_ls
 rec_ls = function()
   return sn(
     nil,
-    c(1, { -- Order is important, sn(...) first would cause infinite loop of expansion.
+    c(1, {
+      -- Order is important, sn(...) first would cause infinite loop of expansion.
       t(""),
       sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
     })
   )
-end
-
--- complicated function for dynamicNode.
-local function jdocsnip(args, _, old_state)
-  -- !!! old_state is used to preserve user-input here. DON'T DO IT THAT WAY!
-  -- Using a restoreNode instead is much easier.
-  -- View this only as an example on how old_state functions.
-  local nodes = {
-    t({ "/**", " * " }),
-    i(1, "A short Description"),
-    t({
-      "",
-      "",
-    }),
-  }
-
-  -- These will be merged with the snippet; that way, should the snippet be updated,
-  -- some user input eg. text can be referred to in the new snippet.
-  local param_nodes = {}
-
-  if old_state then
-    nodes[2] = i(1, old_state.descr:get_text())
-  end
-  param_nodes.descr = nodes[2]
-
-  -- At least one param.
-  if string.find(args[2][1], ", ") then
-    vim.list_extend(nodes, { t({ " * ", "" }) })
-  end
-
-  local insert = 2
-  for indx, arg in ipairs(vim.split(args[2][1], ", ", true)) do
-    -- Get actual name parameter.
-    arg = vim.split(arg, " ", true)[2]
-    if arg then
-      local inode
-      -- if there was some text in this parameter, use it as static_text for this new snippet.
-      if old_state and old_state[arg] then
-        inode = i(insert, old_state["arg" .. arg]:get_text())
-      else
-        inode = i(insert)
-      end
-      vim.list_extend(
-        nodes,
-        { t({ " * @param " .. arg .. " " }), inode, t({ "", "" }) }
-      )
-      param_nodes["arg" .. arg] = inode
-
-      insert = insert + 1
-    end
-  end
-
-  if args[1][1] ~= "void" then
-    local inode
-    if old_state and old_state.ret then
-      inode = i(insert, old_state.ret:get_text())
-    else
-      inode = i(insert)
-    end
-
-    vim.list_extend(
-      nodes,
-      { t({ " * ", " * @return " }), inode, t({ "", "" }) }
-    )
-    param_nodes.ret = inode
-    insert = insert + 1
-  end
-
-  if vim.tbl_count(args[3]) ~= 1 then
-    local exc = string.gsub(args[3][2], " throws ", "")
-    local ins
-    if old_state and old_state.ex then
-      ins = i(insert, old_state.ex:get_text())
-    else
-      ins = i(insert)
-    end
-    vim.list_extend(
-      nodes,
-      { t({ " * ", " * @throws " .. exc .. " " }), ins, t({ "", "" }) }
-    )
-    param_nodes.ex = ins
-    insert = insert + 1
-  end
-
-  vim.list_extend(nodes, { t({ " */" }) })
-
-  local snip = sn(nil, nodes)
-  -- Error on attempting overwrite.
-  snip.old_state = param_nodes
-  return snip
 end
 
 -- Make sure to not pass an invalid command, as io.popen() may write over nvim-text.
@@ -171,14 +94,11 @@ local date_input = function(args, state, fmt)
   return sn(nil, i(1, os.date(fmt)))
 end
 
-ls.filetype_extend("lua", { "c" })
-
-require("luasnip.loaders.from_vscode").load({
-  include = { "go", "lua", "shell" },
-})
-
 ls.snippets = {
-  all = { s("td", { t("// TODO ") }) },
+  all = {
+    s("td", { t("// TODO "), d(1, date_input, {}, "%A, %B %d of %Y") }),
+    s("bash", f(bash, {}, "ls")),
+  },
   go = {
     s("func", {
       t("// "),
@@ -199,9 +119,37 @@ ls.snippets = {
   },
 }
 
+-- autotriggered snippets have to be defined in a separate table, luasnip.autosnippets.
 ls.autosnippets = {
   all = {},
 }
+
+-- in a lua file: search lua-, then c-, then all-snippets.
+ls.filetype_extend("lua", { "c" })
+-- in a cpp file: search c-snippets, then all-snippets only (no cpp-snippets!!).
+ls.filetype_set("cpp", { "c" })
+
+-- Beside defining your own snippets you can also load snippets from "vscode-like" packages
+-- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
+-- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you
+-- will need to extend the table yourself instead of setting a new one.
+
+require("luasnip.loaders.from_vscode").load({}) -- Load only python snippets
+
+-- The directories will have to be structured like eg. <https://github.com/rafamadriz/friendly-snippets> (include
+-- a similar `package.json`)
+-- require("luasnip.loaders.from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
+
+-- You can also use lazy loading so snippets are loaded on-demand, not all at once (may interfere with lazy-loading luasnip itself).
+require("luasnip.loaders.from_vscode").lazy_load() -- You can pass { paths = "./my-snippets/"} as well
+
+-- You can also use snippets in snipmate format, for example <https://github.com/honza/vim-snippets>.
+-- The usage is similar to vscode.
+
+-- One peculiarity of honza/vim-snippets is that the file with the global snippets is _.snippets, so global snippets
+-- are stored in `ls.snippets._`.
+-- We need to tell luasnip that "_" contains global snippets:
+ls.filetype_extend("all", { "_" })
 
 vim.cmd([[
     imap <silent><expr> <C-j> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<down>'
