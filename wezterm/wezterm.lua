91fca58c3109000f6c1d43936b45397c4098e21d
gh-liu <liugh.cs@gmail.com>
2022-02-28 20:03:06 +0800

    wezterm: .

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,32 @@
 local wezterm = require("wezterm")
 
 return {
-	-- default_prog = { "ssh", "128" },
+	font_size = 12.0,
+	-- default_prog = { "ssh", "130" },
 	-- https://wezfurlong.org/wezterm/config/fonts.html
 	font = wezterm.font("JetBrainsMonoNL NF"),
 	-- https://wezfurlong.org/wezterm/colorschemes/index.html
 	color_scheme = "GitHub Dark",
-	enable_scroll_bar = true,
+	colors = {
+		tab_bar = {
+			active_tab = {
+				bg_color = "#24283b",
+				fg_color = "#c0caf5",
+			},
+		},
+	},
+	window_frame = {
+		font_size = 12.0,
+	},
+	window_padding = {
+		left = 5,
+		right = 5,
+		top = 0,
+		bottom = 0,
+	},
+	foreground_text_hsb = {
+		hue = 1.0,
+		saturation = 1.0,
+		brightness = 1.2,
+	},
 }
