270e005f64d41bf1a00f836a203f53160c7f5187
gh-liu <liugh.cs@gmail.com>
2022-05-20 14:29:05 +0800

    nvim: .

--- nvim/lua/modules/plugins/null-ls.lua
+++ nvim/lua/modules/plugins/null-ls.lua
@@ -18,12 +18,43 @@ local sources = {
   -- b.formatting.goimports,
 
   b.diagnostics.golangci_lint,
-
-  b.code_actions.gitsigns,
 }
 
 null_ls.setup({ sources = sources })
 
+local methods = require("null-ls.methods")
+local CODE_ACTION = methods.internal.CODE_ACTION
+local git_sign = {
+  method = CODE_ACTION,
+  filetypes = {},
+  generator = {
+    fn = function(params)
+      local ok, gitsigns_actions = pcall(require("gitsigns").get_actions)
+      if not ok or not gitsigns_actions then
+        return
+      end
+
+      local name_to_title = function(name)
+        return name:sub(1, 1):upper() .. name:gsub("_", " "):sub(2)
+      end
+
+      local actions = {}
+      for name, action in pairs(gitsigns_actions) do
+        -- I do not need the blame line action
+        if name ~= "blame_line" then
+          table.insert(actions, {
+            title = name_to_title(name),
+            action = function()
+              vim.api.nvim_buf_call(params.bufnr, action)
+            end,
+          })
+        end
+      end
+      return actions
+    end,
+  }
+}
+null_ls.register(git_sign)
 
 -- local methods = require("null-ls.methods")
 -- local DIAGNOSTICS_ON_SAVE = methods.internal.DIAGNOSTICS_ON_SAVE
