c3962bc7fb327dcaa2f51979e361d8f2cb4a812a
gh-liu <liugh.cs@gmail.com>
2022-07-14 09:32:21 +0800

    nvim: add lua snippets

---
+++ nvim/lua/modules/plugins/luasnip/snippets/lua.lua
@@ -0,0 +1,15 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+	s("pcall", {
+		t({ "local ok, _ = pcall(require, '" }),
+		i(1),
+		t({ "')", "if not ok then", "\t" }),
+		i(0),
+		t({ "", "end" }),
+	}),
+}
