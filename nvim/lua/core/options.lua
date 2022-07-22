5e8a6194baae5fe0654d0eb4f00a6ab548d4f878
gh-liu <liugh.cs@gmail.com>
2022-07-22 21:29:42 +0800

    nvim: .

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,110 +1,229 @@
-local o, wo, bo = vim.o, vim.wo, vim.bo
+local fn = vim.fn
+local opt = vim.opt
 
-local opt = as.opt
-
-local cmd = vim.api.nvim_command
-
--- Leader/local leader
 vim.g.mapleader = [[,]]
 vim.g.maplocalleader = [[,]]
+vim.api.nvim_command("filetype plugin indent on")
 
-local buffer = { o, bo }
-local window = { o, wo }
+-----------------------------------------------------------------------------//
+-- Message output on vim actions {{{1
+-----------------------------------------------------------------------------//
+opt.shortmess = {
+  t = true, -- truncate file messages at start
+  A = true, -- ignore annoying swap file messages
+  o = true, -- file-read message overwrites previous
+  O = true, -- file-read message overwrites previous
+  T = true, -- truncate non-file messages in middle
+  f = true, -- (file x of x) instead of just (x of x
+  F = true, -- Don't give file info when editing a file, NOTE: this breaks autocommand messages
+  s = true,
+  c = true,
+  W = true, -- Don't show [w] or written when writing
+}
+-----------------------------------------------------------------------------//
+-- Timings {{{1
+-----------------------------------------------------------------------------//
+opt.updatetime = 300
+opt.timeout = true
+opt.timeoutlen = 500
+opt.ttimeoutlen = 10
+-----------------------------------------------------------------------------//
+-- Window splitting and buffers {{{1
+-----------------------------------------------------------------------------//
+opt.splitbelow = true
+opt.splitright = true
+opt.eadirection = "hor"
+-- exclude usetab as we do not want to jump to buffers in already open tabs
+-- do not use split or vsplit to ensure we don't open any new windows
+vim.o.switchbuf = "useopen,uselast"
+opt.fillchars = {
+  fold = " ",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  diff = "╱", -- alternatives = ⣿ ░ ─
+  msgsep = " ", -- alternatives: ‾ ─
+  foldopen = "▾",
+  foldsep = "│",
+  foldclose = "▸",
+}
+-----------------------------------------------------------------------------//
+-- Diff {{{1
+-----------------------------------------------------------------------------//
+-- Use in vertical diff mode, blank lines to keep sides aligned, Ignore whitespace changes
+opt.diffopt = opt.diffopt
+  + {
+    "vertical",
+    "iwhite",
+    "hiddenoff",
+    "foldcolumn:0",
+    "context:4",
+    "algorithm:histogram",
+    "indent-heuristic",
+  }
+-----------------------------------------------------------------------------//
+-- Format Options {{{1
+-----------------------------------------------------------------------------//
+opt.formatoptions = {
+  ["1"] = true,
+  ["2"] = true, -- Use indent from 2nd line of a paragraph
+  q = true, -- continue comments with gq"
+  c = true, -- Auto-wrap comments using textwidth
+  r = true, -- Continue comments when pressing Enter
+  n = true, -- Recognize numbered lists
+  t = false, -- autowrap lines using text width value
+  j = true, -- remove a comment leader when joining lines.
+  -- Only break if the line was not longer than 'textwidth' when the insert
+  -- started and only at a white character that has been entered during the
+  -- current insert command.
+  l = true,
+  v = true,
+}
+-----------------------------------------------------------------------------//
+-- Folds {{{1
+-----------------------------------------------------------------------------//
+-- opt.foldopen = opt.foldopen + 'search'
+-- opt.foldlevelstart = 3
+-- opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- opt.foldmethod = 'expr'
+-----------------------------------------------------------------------------//
+-- Grepprg {{{1
+-----------------------------------------------------------------------------//
+-- Use faster grep alternatives if possible
+if vim.fn.executable("rg") then
+  vim.o.grepprg = [[rg --glob "!.git" --no-heading --vimgrep --follow $*]]
+  opt.grepformat = opt.grepformat ^ { "%f:%l:%c:%m" }
+elseif vim.fn.executable("ag") then
+  vim.o.grepprg = [[ag --nogroup --nocolor --vimgrep]]
+  opt.grepformat = opt.grepformat ^ { "%f:%l:%c:%m" }
+end
+-----------------------------------------------------------------------------//
+-- Wild and file globbing stuff in command mode {{{1
+-----------------------------------------------------------------------------//
+-- opt.wildcharm = fn.char2nr(as.replace_termcodes([[<Tab>]]))
+opt.wildmode = "longest:full,full" -- Shows a menu bar as opposed to an enormous list
+opt.wildignorecase = true -- Ignore case when completing file names and directories
+-- Binary
+opt.wildignore = {
+  "*.out",
+  "*.o",
+  "*.obj",
+  "*.class",
+}
+opt.wildoptions = "pum"
+opt.pumblend = 3 -- Make popup window translucent
+-----------------------------------------------------------------------------//
+-- Display {{{1
+-----------------------------------------------------------------------------//
+opt.conceallevel = 2
+opt.breakindentopt = "sbr"
+opt.linebreak = true -- lines wrap at words rather than random characters
+opt.synmaxcol = 1024 -- don't syntax highlight long lines
+opt.signcolumn = "auto:2-5"
+opt.number = true
+opt.relativenumber = true
+opt.ruler = false
+opt.cmdheight = 2 -- Set command line height to two lines
+opt.showbreak = [[↪ ]] -- Options include -> '…', '↳ ', '→','↪ '
+opt.cc = "80"
+-----------------------------------------------------------------------------//
+-- List chars {{{1
+-----------------------------------------------------------------------------//
+opt.list = true -- invisible chars
+opt.listchars = {
+  eol = nil, -- eol = "↴",
+  tab = "  ", -- Alternatives: '▷▷', -- tab = "▸\\",
+  extends = "›", -- Alternatives: … »
+  precedes = "‹", -- Alternatives: … «
+  trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
+  space = "⋅",
+}
+-----------------------------------------------------------------------------//
+-- Indentation
+-----------------------------------------------------------------------------//
+opt.wrap = true
+opt.wrapmargin = 2
+opt.textwidth = 80
+opt.autoindent = true
+opt.shiftround = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.whichwrap = "b,s,<,>,h,l"
+-----------------------------------------------------------------------------//
+opt.gdefault = true
+opt.pumheight = 15
+opt.confirm = true -- make vim prompt me to save before doing destructive things
+-- opt.completeopt = { 'menuone', 'noselect' }
+opt.hlsearch = true
+opt.autowriteall = true -- automatically :write before running commands and changing files
+opt.clipboard = { "unnamedplus" }
+opt.laststatus = 3
+opt.termguicolors = true
+-- opt.guifont = 'Fira Code Regular Nerd Font Complete Mono:h14'
+-----------------------------------------------------------------------------//
+-- Cursor {{{1
+-----------------------------------------------------------------------------//
+-- This is from the help docs, it enables mode shapes, "Cursor" highlight, and blinking
+opt.guicursor = {
+  [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]],
+  [[a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor]],
+  [[sm:block-blinkwait175-blinkoff150-blinkon175]],
+}
 
--- MISC
-opt("hidden", true) -- Hide unused buffers
-opt("cc", 80) -- Set an 80 column boarder for good coding style
--- opt("lazyredraw", true)
-opt("updatetime", 200)
-opt("textwidth", 80)
-
--- PANE
-opt("splitbelow", true) -- Always add new pane below
-opt("splitright", true) -- Always add new pane on right
-
--- search and replace
-opt("ignorecase", true) -- Case insensitive matching
-opt("smartcase", true)
-opt("hlsearch", true) -- Highlight search results
-opt("inccommand", "split") -- Show replace result in a split screen before applying
-opt("magic", true)
-opt("incsearch", true)
-opt("synmaxcol", 500, buffer)
-
--- tab and indentation
-opt("tabstop", 2, buffer) -- Number of columns occupied by a tab character
-opt("shiftwidth", 2, buffer) -- Width for
-opt("softtabstop", 0, buffer) -- How far cursor travels by pressing tab
-opt("expandtab", true, buffer) -- Converts tab to whitespace
-opt("autoindent", true, buffer) -- Indent a new line the same amound as the line before it
--- opt("smartindent", true, buffer)
-
--- mouse and cursor
-opt("mouse", "nivh")
-opt("cursorline", true, window) -- Highlight current cursorline
-opt("guicursor", [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50]])
--- scrolloff
-opt("scrolloff", 7)
-opt("sidescrolloff", 7)
-
--- sign column
-opt("signcolumn", "yes:1", window)
--- line number
-opt("number", true, window)
-opt("relativenumber", true, window)
-
--- TERM SETTINGS
-opt("termguicolors", true)
-
--- fold
-opt("foldlevel", 999)
-opt("foldmethod", "indent")
-
--- status line
-opt("laststatus", 3)
-opt("showmode", false)
-opt("modeline", false, buffer)
-
--- wrap
-opt("wrap", false)
-opt("whichwrap", vim.o.whichwrap .. "<,>,h,l")
-
--- popup menu
-opt("pumheight", 12)
--- opt('completeopt', 'menuone,noselect') -- Set completeopt to have a better completion experience
-
--- conceal
--- opt('conceallevel', 2, window)
-opt("concealcursor", "nc", window)
-
--- filetype
-cmd("filetype plugin indent on")
-
--- vim.opt.list = true
--- vim.opt.listchars = {
--- 	tab = "▸\\",
--- 	space = "⋅",
--- 	eol = "↴",
+opt.cursorline = true
+opt.cursorlineopt = "screenline,number"
+-----------------------------------------------------------------------------//
+-- Title {{{1
+-----------------------------------------------------------------------------//
+-- opt.titlestring = ' ❐ %{fnamemodify(getcwd(), ":t")}'
+-- opt.titleold = fn.fnamemodify(vim.loop.os_getenv('SHELL'), ':t')
+-- opt.title = true
+-- opt.titlelen = 70
+-----------------------------------------------------------------------------//
+-- Utilities {{{1
+-----------------------------------------------------------------------------//
+opt.showmode = false
+-- NOTE: Don't remember help files since that will error if they are from a lazy loaded plugin
+-- opt.sessionoptions = {
+--   'globals',
+--   'buffers',
+--   'curdir',
+--   'winpos',
+--   'tabpages',
 -- }
+opt.viewoptions = { "cursor", "folds" } -- save/restore just these (with `:{mk,load}view`)
+opt.virtualedit = "block" -- allow cursor to move where there is no text in visual block mode
+--}}}
+-------------------------------------------------------------------------------
+-- BACKUP AND SWAPS {{{
+-------------------------------------------------------------------------------
+opt.backup = false
+opt.undofile = true
+opt.swapfile = false
+--}}}
+-----------------------------------------------------------------------------//
+-- Match and search {{{1
+-----------------------------------------------------------------------------//
+opt.ignorecase = true
+opt.smartcase = true
+opt.wrapscan = true -- Searches wrap around the end of the file
+opt.scrolloff = 9
+opt.sidescrolloff = 10
+opt.sidescroll = 1
+--}}}
+-----------------------------------------------------------------------------//
+-- Spelling {{{1
+-----------------------------------------------------------------------------//
+-- opt.spellsuggest:prepend({ 12 })
+-- opt.spelloptions = 'camel'
+-- opt.spellcapcheck = '' -- don't check for capital letters at start of sentence
+-- opt.fileformats = { 'unix', 'mac', 'dos' }
+-- opt.spelllang:append('programming')
+--}}}
+-----------------------------------------------------------------------------//
+-- Mouse {{{1
+-----------------------------------------------------------------------------//
+opt.mouse = "a"
+opt.mousefocus = true
+--}}}
+-----------------------------------------------------------------------------//
 
-opt("undofile", true, buffer)
-opt("previewheight", 5)
-opt("display", "msgsep")
-opt("showmatch", true)
-opt("shortmess", o.shortmess .. "c")
-
--- ToggleMouse = function()
---   if vim.o.mouse == 'a' then
---     vim.wo.signcolumn = 'no'
---     vim.o.mouse = 'v'
---     vim.wo.number = false
---     print 'Mouse disabled'
---   else
---     vim.wo.signcolumn = 'yes'
---     vim.o.mouse = 'a'
---     vim.wo.number = true
---     print 'Mouse enabled'
---   end
--- end
---
--- vim.keymap.set('n', '<leader>bm', ToggleMouse)
+-- vim:foldmethod=marker
