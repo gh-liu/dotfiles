31f962b507ffc8c82225cab596513ed5aba1cb53
liugh <liugh.cs@gmail.com>
2023-09-17 16:00:50 +0800

    feat(nvim): add nvim-osc52

--- xdg_config/nvim/lua/liu/init.lua
+++ xdg_config/nvim/lua/liu/init.lua
@@ -1365,7 +1365,36 @@ require("lazy").setup(
 
 			-- config = function() end,
 		},
+		{
+			"ojroques/nvim-osc52",
+			cond = function()
+				return os.getenv("SSH_CLIENT")
+			end,
+			config = function(_, opts)
+				require("osc52").setup({
+					max_length = 0, -- Maximum length of selection (0 for no limit)
+					silent = false, -- Disable message on successful copy
+					trim = false, -- Trim surrounding whitespaces before copy
+				})
 
+				local function copy(lines, _)
+					require("osc52").copy(table.concat(lines, "\n"))
+				end
+
+				local function paste()
+					return {
+						vim.fn.split(vim.fn.getreg(""), "\n"),
+						vim.fn.getregtype(""),
+					}
+				end
+
+				vim.g.clipboard = {
+					name = "osc52",
+					copy = { ["+"] = copy, ["*"] = copy },
+					paste = { ["+"] = paste, ["*"] = paste },
+				}
+			end,
+		},
 		-- }}}
 
 		-- Lang {{{2
