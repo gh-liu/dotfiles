431406c9aa007689a7505a0228e0e72fcc6de5b1
gh-liu <liugh.cs@gmail.com>
2023-10-19 19:46:25 +0800

    feat(nvim): update injection query

--- xdg_config/nvim/queries/lua/injections.scm
+++ xdg_config/nvim/queries/lua/injections.scm
@@ -1,11 +1,11 @@
 ; extends
 
 (function_call 
-	name: (dot_index_expression  
-                field: (identifier)@_prefix_1 .)
-	arguments: (arguments 
-                     (string content: (string_content)@injection.content) .)
-        (#match? @_prefix_1 "set|parse_query")
-        (#set! injection.language "query")
-        )
+  name: (dot_index_expression  
+          field: (identifier)@_prefix_1 .)
+  arguments: (arguments 
+               (string content: (_)@injection.content) .)
+  (#any-of? @_prefix_1 "parse_query")
+  (#set! injection.language "query")
+  )
 
