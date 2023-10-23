dbe07351b86c41c199a7a188b64bf4cfdc4c98ec
gh-liu <liugh.cs@gmail.com>
2023-10-23 19:48:06 +0800

    feat(nvim): map for terminal

--- xdg_config/nvim/lua/liu/init.lua
+++ xdg_config/nvim/lua/liu/init.lua
@@ -2199,7 +2199,20 @@ autocmd({ "BufWritePre" }, {
 		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
 	end,
 })
-
+autocmd({ "TermOpen" }, {
+	group = user_augroup("term_map"),
+	pattern = "term://*",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+		vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+		vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+		vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+		vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+		vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+		vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+	end,
+})
 -- }}}
 
 -- Diagnostic {{{1
