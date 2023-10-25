51f3f31772c24e91db4e0845108b25787f79841c
gh-liu <liugh.cs@gmail.com>
2023-10-25 10:10:47 +0800

    feat(nvim): remove libmodal in statusline

--- xdg_config/nvim/after/plugin/statusline.lua
+++ xdg_config/nvim/after/plugin/statusline.lua
@@ -91,18 +91,9 @@ local ViMode = {
 		},
 	},
 	provider = function(self)
-		if vim.g.libmodalActiveModeName then
-			return ("%2(" .. vim.g.libmodalActiveModeName .. "%)")
-		end
-
 		return ("%2(" .. self.mode_names[self.mode] .. "%)")
 	end,
 	hl = function(self)
-		if vim.g.libmodalActiveModeName then
-			local mode = vim.g.libmodalActiveModeName
-			return { fg = "cyan", bold = true }
-		end
-
 		local mode = self.mode:sub(1, 1)
 		return { fg = self.mode_colors[mode], bold = true }
 	end,
@@ -397,18 +388,6 @@ local LeapSign = {
 	},
 }
 
-local LibmodalLayer = {
-	condition = function()
-		return vim.g.libmodalActiveLayerName
-	end,
-	{
-		provider = function()
-			return "*" .. vim.g.libmodalActiveLayerName .. "*"
-		end,
-		hl = { fg = "yellow", bg = "back_ground", bold = true },
-	},
-}
-
 local DAPMessages = {
 	condition = function()
 		return vim.g.debuging and require("dap").session()
@@ -468,7 +447,6 @@ local DefaultStatusline = {
 	Git,
 	Align,
 	LeapSign,
-	LibmodalLayer,
 	Space,
 	DAPMessages,
 	Space,
