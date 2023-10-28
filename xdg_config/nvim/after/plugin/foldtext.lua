ca790997a903a3dccf1dce36544285ff9ba78ab8
liugh <liugh.cs@gmail.com>
2023-10-28 10:34:41 +0800

    feat(nvim): update foldtext and delete foldtext.vim

--- xdg_config/nvim/after/plugin/foldtext.lua
+++ xdg_config/nvim/after/plugin/foldtext.lua
@@ -1,10 +1,21 @@
+-- " :h fold-foldtext
+-- " v:foldstart	line number of first line in the fold
+-- " v:foldend	line number of last line in the fold
+-- " v:folddashes	a string that contains dashes to represent the foldlevel.
+-- " v:foldlevel	the foldlevel of the fold
+
 local function foldtext()
 	local result = vim.treesitter.foldtext()
 	if type(result) == "table" then
+	else
+		result = {}
+		local start_line = vim.fn.getline(vim.v.foldstart)
+		table.insert(result, { start_line, "Folded" })
+
+	end
 		table.insert(result, { " ...", "NonText" })
 		table.insert(result, { string.format(" +%s(%s)", vim.v.folddashes, vim.v.foldlevel), "NonText" })
 		table.insert(result, { string.format(" [%d lines]", vim.v.foldend - vim.v.foldstart), "NonText" })
-	end
 	return result
 end
 
