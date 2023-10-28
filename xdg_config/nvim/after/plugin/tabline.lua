340dc5bc7ce403a0c9e5fa96e4bd0eac80b64c66
liugh <liugh.cs@gmail.com>
2023-10-28 10:06:51 +0800

    feat(nvim): update tabline

--- xdg_config/nvim/after/plugin/tabline.lua
+++ xdg_config/nvim/after/plugin/tabline.lua
@@ -1,9 +1,22 @@
-local extract_highlight_colors = function(color_group, scope)
-	if vim.fn.hlexists(color_group) == 0 then
+if false then
+	return
+end
+
+local M = {}
+
+---@class color
+---@field fg string
+---@field bg string
+
+---extract highlight
+---@param hl string
+---@return color|nil
+local extract_highlight_colors = function(hl)
+	if vim.fn.hlexists(hl) == 0 then
 		return nil
 	end
 
-	local color = vim.api.nvim_get_hl(0, { name = color_group })
+	local color = vim.api.nvim_get_hl(0, { name = hl })
 	if color.background ~= nil then
 		color.bg = string.format("#%06x", color.background)
 		color.background = nil
@@ -12,56 +25,72 @@ local extract_highlight_colors = function(color_group, scope)
 		color.fg = string.format("#%06x", color.foreground)
 		color.foreground = nil
 	end
-
-	if scope then
-		return color[scope]
-	end
 	return color
 end
 
+---create a highlight for specificed filetype and return the highlight name
+---@param color color
+---@param ft string
+---@return string
 local create_highlight_group = function(color, ft)
 	if color.bg and color.fg then
-		local highlight_group_name = table.concat({ "user", "tab", ft }, "_")
+		local highlight_group_name = table.concat({ "user", "tab", "hl", ft }, "_")
 		vim.api.nvim_set_hl(0, highlight_group_name, { fg = color.fg, bg = color.bg })
 		return highlight_group_name
 	end
+	return "Normal"
 end
-local icon = function(bufnr, isSelected)
+
+---Get the icon
+---@param bufnr number
+---@param isSelected boolean
+---@return string
+M.icon = function(bufnr, isSelected)
 	local filetype = vim.fn.getbufvar(bufnr, "&filetype")
 	if filetype == "" then
 		return ""
 	end
 
-	local icon, color = require("nvim-web-devicons").get_icon_color_by_filetype(filetype, { default = true })
+	local icon, icon_color = require("nvim-web-devicons").get_icon_color_by_filetype(filetype, { default = true })
 
-	local bg = extract_highlight_colors("TabLineSel", "bg")
-	local hl = create_highlight_group({ bg = bg, fg = color }, filetype)
+	local tabline_colors = extract_highlight_colors("TabLineSel")
+	local hl = create_highlight_group({ bg = tabline_colors.bg, fg = icon_color }, filetype)
 	local selectedHlStart = (isSelected and hl) and "%#" .. hl .. "#" or ""
 	local selectedHlEnd = isSelected and "%#TabLineSel#" or ""
 
 	return selectedHlStart .. icon .. selectedHlEnd .. " "
 end
 
-local title = function(bufnr)
-	local bufname = vim.fn.bufname(bufnr)
-	local file = vim.fn.fnamemodify(bufname, ":t")
+---Get the title
+---@param bufnr number
+---@return string
+M.title = function(bufnr)
 	local buftype = vim.fn.getbufvar(bufnr, "&buftype")
 	local filetype = vim.fn.getbufvar(bufnr, "&filetype")
 
-	if buftype == "help" then
-		return "Help: " .. file
-	end
-
+	-- special buftypes
 	local buftypes = {
 		"prompt",
 		"quickfix",
 	}
-	for _, btp in ipairs(buftypes) do
-		if buftype:find(btp) then
+	for _, bt in ipairs(buftypes) do
+		if buftype:find(bt) then
 			return (buftype:gsub("^%l", string.upper))
 		end
 	end
 
+	-- file name
+	local bufname = vim.fn.bufname(bufnr)
+	if bufname == "" then
+		return "[No Name]"
+	end
+	local file_name = vim.fn.fnamemodify(bufname, ":t")
+
+	if buftype == "help" then
+		return "Help: " .. file_name
+	end
+
+	-- special filetypes
 	local filetypes = {
 		"^git.*",
 		"oil",
@@ -75,16 +104,13 @@ local title = function(bufnr)
 		end
 	end
 
-	if bufname == "" then
-		return "[No Name]"
-	end
-
-	-- vim.print(file)
-
-	return file
+	return file_name or ""
 end
 
-local modified = function(bufnr)
+---Get modified sign
+---@param bufnr number
+---@return string
+M.modified = function(bufnr)
 	return vim.fn.getbufvar(bufnr, "&modified") == 1 and "[+] " or ""
 end
 
@@ -92,7 +118,10 @@ local separator = function(index)
 	return (index < vim.fn.tabpagenr("$") and "%#TabLine#|" or "")
 end
 
-local tab = function(index)
+---single table item
+---@param index number
+---@return string
+M.tab = function(index)
 	local winnr = vim.fn.tabpagewinnr(index)
 	local bufnr = vim.fn.tabpagebuflist(index)[winnr]
 
@@ -104,28 +133,24 @@ local tab = function(index)
 		.. index
 		.. "T"
 		.. " "
-		.. icon(bufnr, isSelected)
-		.. title(bufnr)
+		.. M.icon(bufnr, isSelected)
+		.. M.title(bufnr)
 		.. " "
-		.. modified(bufnr)
+		.. M.modified(bufnr)
 		.. "%T"
 		.. separator(index)
 end
 
-local tabline = function()
+M.render = function()
 	local last_index = vim.fn.tabpagenr("$")
-
-	local line = ""
+	local tabs = ""
 	for index = 1, last_index do
-		line = line .. tab(index)
+		tabs = tabs .. M.tab(index)
 	end
-	line = line .. "%#TabLineFill#%="
-
-	return line
+	tabs = tabs .. "%#TabLineFill#%="
+	return tabs
 end
 
-function _G.nvim_tabline()
-	return tabline()
-end
+_G.nvim_tabline = M.render
 
 vim.opt.tabline = "%!v:lua.nvim_tabline()"
