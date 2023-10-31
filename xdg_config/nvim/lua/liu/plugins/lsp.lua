fddb77879dc298c92ea2d483f0e48cd3481e5859
gh-liu <liugh.cs@gmail.com>
2023-10-31 11:44:26 +0800

    perf(nvim): didChangeWatchedFiles is too slow

--- xdg_config/nvim/lua/liu/plugins/lsp.lua
+++ xdg_config/nvim/lua/liu/plugins/lsp.lua
@@ -163,8 +163,19 @@ local on_attach = function(client, bufnr)
 	-- vim.print(client.server_capabilities.executeCommandProvider)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local capabilities = vim.tbl_deep_extend(
+	"force",
+	vim.lsp.protocol.make_client_capabilities(),
+	-- nvim-cmp supports additional completion capabilities, so broadcast that to servers.
+	require("cmp_nvim_lsp").default_capabilities(),
+	{
+		workspace = {
+			-- PERF: didChangeWatchedFiles is too slow.
+			-- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
+			didChangeWatchedFiles = { dynamicRegistration = false },
+		},
+	}
+)
 
 local handlers = {}
 
