d80480cead1f18efb851e9257b99dedeed43c93d
gh-liu <liugh.cs@gmail.com>
2023-11-01 10:08:31 +0800

    feat(nvim): support osc52

---
+++ xdg_config/nvim/after/plugin/clipboard.lua
@@ -0,0 +1,19 @@
+if false then
+	return
+end
+
+-- disable in TMUX
+if os.getenv("TMUX") then
+	return
+end
+
+local function osc52_copy(text)
+	local osc = string.format("%s]52;c;%s%s", string.char(0x1b), vim.base64.encode(text), string.char(0x07))
+	io.stderr:write(osc)
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		osc52_copy(vim.fn.getreg(vim.v.event.regname))
+	end,
+})
