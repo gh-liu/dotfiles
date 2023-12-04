3e8ac748fc103ad726d2c237d4223d15aeccee67
gh-liu <liugh.cs@gmail.com>
2023-12-04 13:53:41 +0800

    feat(nvim): use bufdelete.nvim instead of mini.bufremove

--- xdg_config/nvim/lua/liu/init.lua
+++ xdg_config/nvim/lua/liu/init.lua
@@ -1670,35 +1670,17 @@ require("lazy").setup(
 			},
 			cmd = "Glow",
 		},
-
 		{
-			"echasnovski/mini.bufremove",
+			"famiu/bufdelete.nvim",
+			-- event = "VeryLazy",
+			cmd = { "Bdelete", "Bwipeout" },
 			keys = {
 				{
 					"<leader>bd",
 					function()
-						local bd = require("mini.bufremove").delete
-						if vim.bo.modified then
-							local choice =
-								vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-							if choice == 1 then -- Yes
-								vim.cmd.write()
-								bd(0)
-							elseif choice == 2 then -- No
-								bd(0, true)
-							end
-						else
-							bd(0)
-						end
+						require("bufdelete").bufdelete(0, false)
 					end,
 					desc = "Delete Buffer",
-				},
-				{
-					"<leader>bD",
-					function()
-						require("mini.bufremove").delete(0, true)
-					end,
-					desc = "Delete Buffer (Force)",
 				},
 			},
 		},
