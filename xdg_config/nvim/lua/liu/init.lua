adec9fb37fd9328ad4d90d5d08069b2c5689f2bd
gh-liu <liugh.cs@gmail.com>
2023-12-05 13:55:15 +0800

    feat(nvim): update mini.files

--- xdg_config/nvim/lua/liu/init.lua
+++ xdg_config/nvim/lua/liu/init.lua
@@ -1517,13 +1517,21 @@ require("lazy").setup(
 			lazy = true,
 			keys = {
 				{
-					"-",
+					"<leader>e",
 					function()
 						local MiniFiles = require("mini.files")
 						if not MiniFiles.close() then
-							MiniFiles.open(vim.api.nvim_buf_get_name(0))
+							-- MiniFiles.open(vim.api.nvim_buf_get_name(0))
+							local bufname = vim.api.nvim_buf_get_name(0)
+							local path = vim.fn.fnamemodify(bufname, ":p")
+
+							-- Noop if the buffer isn't valid.
+							if path and vim.uv.fs_stat(path) then
+								require("mini.files").open(bufname, false)
+							end
 						end
 					end,
+					desc = "File [E]xplorer",
 				},
 			},
 			init = function()
