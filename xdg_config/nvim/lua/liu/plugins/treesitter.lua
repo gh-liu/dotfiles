7a8e8dbb0e2f990caa5d332e64dd803dcc528470
gh-liu <liugh.cs@gmail.com>
2023-12-05 15:52:08 +0800

    feat(nvim): update treesitter

--- xdg_config/nvim/lua/liu/plugins/treesitter.lua
+++ xdg_config/nvim/lua/liu/plugins/treesitter.lua
@@ -26,13 +26,15 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 	highlight = {
 		enable = true,
-		-- Disable slow treesitter highlight for large files
-		disable = function(lang, buf)
-			local max_filesize = 64 * 1024 -- 64 KB
-			local ok, stats = pcall(vim.loop.fs_stat, api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
+		disable = function(_, buf)
+			-- Don't disable for read-only buffers.
+			if not vim.bo[buf].modifiable then
+				return false
 			end
+
+			local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+			-- Disable for files larger than 256 KB.
+			return ok and stats and stats.size > (256 * 1024)
 		end,
 	},
 	indent = { enable = true },
