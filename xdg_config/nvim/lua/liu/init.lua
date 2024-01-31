9ea504daa9754d371949ef78277e3cf729fbad09
liugh <liugh.cs@gmail.com>
2024-01-31 10:34:15 +0800

    feat(nvim): add DiffOrig command

--- xdg_config/nvim/lua/liu/init.lua
+++ xdg_config/nvim/lua/liu/init.lua
@@ -2028,6 +2028,27 @@ create_command("AppendModeline", function(opts)
 	api.nvim_exec2("doautocmd BufRead", {})
 end, {})
 
+-- :h DiffOrig
+create_command("DiffOrig", function()
+	-- Get start buffer
+	local start = api.nvim_get_current_buf()
+
+	-- `vnew` - Create empty vertical split window
+	-- `set buftype=nofile` - Buffer is not related to a file, will not be written
+	-- `0d_` - Remove an extra empty start row
+	-- `diffthis` - Set diff mode to a new vertical split
+	vim.cmd("vnew | set buftype=nofile | read ++edit # | 0d_ | diffthis")
+
+	-- Get scratch buffer
+	local scratch = api.nvim_get_current_buf()
+
+	-- some filetype
+	vim.bo[scratch].ft = vim.bo[start].ft
+
+	-- `wincmd p` - Go to the start window
+	-- `diffthis` - Set diff mode to a start window
+	vim.cmd("wincmd p | diffthis")
+end, {})
 -- }}}
 
 -- Autocmds {{{1
