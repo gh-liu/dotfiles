b82459bf201b09d5693e0af72b8dfbdcb3c1a614
liugh <liugh.cs@gmail.com>
2024-03-02 12:07:43 +0800

    feat(nvim): lsp watch files

--- xdg_config/nvim/lua/liu/plugins/lsp.lua
+++ xdg_config/nvim/lua/liu/plugins/lsp.lua
@@ -266,14 +266,14 @@ local capabilities = vim.tbl_deep_extend(
 	"force",
 	vim.lsp.protocol.make_client_capabilities(),
 	-- nvim-cmp supports additional completion capabilities, so broadcast that to servers.
-	require("cmp_nvim_lsp").default_capabilities(),
-	{
-		workspace = {
-			-- PERF: didChangeWatchedFiles is too slow.
-			-- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
-			didChangeWatchedFiles = { dynamicRegistration = false },
-		},
-	}
+	require("cmp_nvim_lsp").default_capabilities()
+	-- {
+	-- 	workspace = {
+	-- 		-- PERF: didChangeWatchedFiles is too slow.
+	-- 		-- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
+	-- 		didChangeWatchedFiles = { dynamicRegistration = false },
+	-- 	},
+	-- }
 )
 
 for server_name, server_config in pairs(servers) do
