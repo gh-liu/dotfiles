46655d5fb498b09f1accefcaed26b04ec7fe7979
gh-liu <liugh.cs@gmail.com>
2024-03-15 09:44:45 +0800

    fix(nvim): treesitter for folding

--- xdg_config/nvim/lua/liu/plugins/treesitter.lua
+++ xdg_config/nvim/lua/liu/plugins/treesitter.lua
@@ -41,34 +41,50 @@ vim.keymap.set("n", "<leader>rn", edit.smart_rename)
 local ts_foldexpr = "v:lua.vim.treesitter.foldexpr()"
 local ts_foldtext = ""
 
-local function set_ts_win_defaults(ev)
-	local bufnr = ev.buf
-	if not vim.b.is_foldable then
-		local lang = ts.language.get_lang(api.nvim_get_option_value("filetype", { buf = bufnr }))
-		if parsers.has_parser(lang) then
-			local ok, has_folds = pcall(ts.query.get, lang, "folds")
-			if ok and has_folds then
-				vim.b.is_foldable = true
-			end
+local is_foldable = function(buf)
+	buf = buf or api.nvim_get_current_buf()
+	local lang = ts.language.get_lang(api.nvim_get_option_value("filetype", { buf = buf }))
+	if parsers.has_parser(lang) then
+		local ok, has_folds = pcall(ts.query.get, lang, "folds")
+		if ok and has_folds then
+			return true
 		end
+	end
+	return false
+end
+
+local set_treesitter_fold = function(win)
+	-- Like `:setlocal` if {bufnr} is provided
+	local cur_win_opt = vim.wo[win][0]
+	-- if current `foldmethod` is `manual`, then changed to `expr` and set `foldexpr`
+	if cur_win_opt.foldmethod == "manual" then
+		cur_win_opt.foldmethod = "expr"
+		cur_win_opt.foldexpr = ts_foldexpr
+	end
+
+	-- if current `foldtext` is `foldtext()`, then change the foldtext
+	if cur_win_opt.foldtext == "foldtext()" then
+		cur_win_opt.foldtext = ts_foldtext
+	end
+end
+
+local function set_ts_win_defaults(ev)
+	local buf = ev.buf
+	if not vim.b.is_foldable then
+		vim.b.is_foldable = is_foldable(buf)
 	end
 
 	if vim.b.is_foldable then
 		local winid = api.nvim_get_current_win()
-		-- Like `:setlocal` if {bufnr} is provided
-		local cur_win_opt = vim.wo[winid][0]
-		if cur_win_opt.foldmethod == "manual" then
-			cur_win_opt.foldmethod = "expr"
-			cur_win_opt.foldexpr = ts_foldexpr
-		end
-		if cur_win_opt.foldtext == "foldtext()" then
-			cur_win_opt.foldtext = ts_foldtext
-		end
+		set_treesitter_fold(winid)
 	end
 end
 
-api.nvim_create_autocmd({ "BufWinEnter" }, {
+api.nvim_create_autocmd({
+	"BufReadPost",
+	"WinEnter",
+}, {
 	callback = set_ts_win_defaults,
 	group = api.nvim_create_augroup("liug/ts_fold", { clear = true }),
-	desc = "Set treesitter defaults on win enter",
+	desc = "set treesitter fold",
 })
