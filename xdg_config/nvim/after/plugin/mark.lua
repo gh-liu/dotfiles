b4f080216d6880aa3fbc796e6ef410bc3143f0fe
gh-liu <liugh.cs@gmail.com>
2024-03-22 17:00:08 +0800

    fix(nvim): marks

--- xdg_config/nvim/after/plugin/mark.lua
+++ xdg_config/nvim/after/plugin/mark.lua
@@ -2,175 +2,176 @@ if false then
 	return
 end
 
-local sign_group_name = "liu/marks_signs"
-
-local mark_sign_ns = vim.api.nvim_create_namespace(sign_group_name)
+local fn = vim.fn
+local api = vim.api
 
 local sign_priority = 10
+local sign_hl = "MoreMsg"
+
+local sign_group_name = "liu/marks_signs"
+
+local mark_sign_ns = api.nvim_create_namespace(sign_group_name)
 
 ---@param mark string
 ---@return boolean
-local function is_lowercase_mark(mark)
-	return 97 <= mark:byte() and mark:byte() <= 122
+local function uppercase_letter_mark(mark)
+	-- :h lua-patterns
+	return mark:match("%u")
 end
 
 ---@param mark string
 ---@return boolean
-local function is_uppercase_mark(mark)
-	return 65 <= mark:byte() and mark:byte() <= 90
+local function lowercase_letter_mark(mark)
+	-- :h lua-patterns
+	return mark:match("%l")
 end
 
----@param mark string
----@return boolean
-local function is_letter_mark(mark)
-	return is_lowercase_mark(mark) or is_uppercase_mark(mark)
-end
+local M = {}
 
 --- Map of mark information per buffer.
 ---@type table<integer, table<string, {line: integer, id: integer}>>
-local cache_marks = {}
+M.cache_marks = {}
 
 ---@param mark string
----@param bufnr integer
-local function delete_mark(mark, bufnr)
-	local buffer_marks = cache_marks[bufnr]
-	if not buffer_marks or not buffer_marks[mark] then
+---@param bufnr integer|nil
+M.delete_mark_sign = function(mark, bufnr)
+	bufnr = bufnr or api.nvim_get_current_buf()
+
+	local cache_marks = M.cache_marks[bufnr]
+	-- Mark not exists, just return.
+	if not cache_marks or not cache_marks[mark] then
 		return
 	end
 
-	vim.api.nvim_buf_del_extmark(bufnr, mark_sign_ns, buffer_marks[mark].id)
-	buffer_marks[mark] = nil
+	api.nvim_buf_del_extmark(bufnr, mark_sign_ns, cache_marks[mark].id)
+	cache_marks[mark] = nil
+end
 
+---@param mark string
+---@param bufnr integer|nil
+M.delete_mark = function(mark, bufnr)
+	if bufnr then
+		M.delete_mark_sign(mark, bufnr)
+	else
+		for buf, marks in pairs(M.cache_marks) do
+			if marks[mark] then
+				M.delete_mark_sign(mark, buf)
+			end
+		end
+	end
 	vim.cmd("delmarks " .. mark)
 end
 
 ---@param mark string
----@param bufnr integer
----@param line integer
-local function register_mark(mark, bufnr, line)
-	local buffer_marks = cache_marks[bufnr]
-	if not buffer_marks then
-		return
+---@param bufnr integer|nil
+---@param line integer|nil
+M.add_mark_sign = function(mark, bufnr, line)
+	bufnr = bufnr or api.nvim_get_current_buf()
+	-- Marks of buffer not exists, create cache first.
+	if not M.cache_marks[bufnr] then
+		M.cache_marks[bufnr] = {}
 	end
-
 	-- Mark already exists, remove it first.
-	if buffer_marks[mark] then
-		delete_mark(mark, bufnr)
+	if uppercase_letter_mark(mark) then
+		M.delete_mark(mark)
+	end
+	if lowercase_letter_mark(mark) and M.cache_marks[bufnr][mark] then
+		M.delete_mark(mark, bufnr)
 	end
 
+	line = line or api.nvim_win_get_cursor(0)[1]
 	line = line - 1
-	local id = vim.api.nvim_buf_set_extmark(0, mark_sign_ns, line, 0, {
-		sign_text = mark,
-		sign_hl_group = "MoreMsg",
-		priority = sign_priority,
-	})
-	buffer_marks[mark] = { line = line, id = id }
+	local opts = { sign_text = mark, sign_hl_group = sign_hl, priority = sign_priority }
+	local id = api.nvim_buf_set_extmark(bufnr, mark_sign_ns, line, 0, opts)
+	M.cache_marks[bufnr][mark] = { line = line, id = id }
 end
 
-local map_add_mark = function(bufnr, mark)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
-	register_mark(mark, bufnr, vim.api.nvim_win_get_cursor(0)[1])
+---@param mark string
+---@param bufnr integer|nil
+---@param line integer|nil
+M.add_mark = function(mark, bufnr, line)
+	M.add_mark_sign(mark, bufnr, line)
 	vim.cmd("normal! m" .. mark)
 end
 
-local map_delete_mark = function(bufnr, mark)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
-	delete_mark(mark, bufnr)
-end
-
--- A-Z
-for i = 65, 90, 1 do
-	local c = string.char(i)
-	vim.keymap.set("n", "m" .. c, function()
-		map_add_mark(nil, c)
-	end, {})
-
-	vim.keymap.set("n", "dm" .. c, function()
-		map_delete_mark(nil, c)
-	end, {})
-end
 ---@param bufnr integer
-local function on_attach(bufnr)
-	-- a-z
+local function add_buf_maps(bufnr)
+	local keymap = vim.keymap
+	-- buffer: a-z
 	for i = 97, 122, 1 do
 		local c = string.char(i)
-		vim.keymap.set("n", "m" .. c, function()
-			map_add_mark(bufnr, c)
-		end, {
-			buffer = bufnr,
-		})
+		keymap.set("n", "m" .. c, function()
+			M.add_mark(c, bufnr)
+		end, { buffer = bufnr })
 
-		vim.keymap.set("n", "dm" .. c, function()
-			map_delete_mark(bufnr, c)
-		end, {
-			buffer = bufnr,
-		})
+		keymap.set("n", "dm" .. c, function()
+			M.delete_mark(c, bufnr)
+		end, { buffer = bufnr })
+	end
+end
+
+local function add_global_maps()
+	local keymap = vim.keymap
+	-- global: A-Z
+	for i = 65, 90, 1 do
+		local c = string.char(i)
+		keymap.set("n", "m" .. c, function()
+			M.add_mark(c)
+		end, { desc = "mark '" .. c })
+
+		keymap.set("n", "dm" .. c, function()
+			M.delete_mark(c)
+		end, { desc = "delete mark '" .. c })
 	end
 
-	vim.keymap.set("n", "M", "g`", {
-		desc = "Jump to the exact location of a mark",
-		buffer = bufnr,
+	keymap.set("n", "M", "g`", { desc = "Jump to the exact location of a mark" })
+end
+
+local register_marks = function(marks, bufnr)
+	bufnr = bufnr or api.nvim_get_current_buf()
+	for _, data in ipairs(marks) do
+		local mark = data.mark:sub(2, 3) -- name of the mark prefixed by "'"
+		local mark_buf, mark_line = unpack(data.pos) -- [bufnum, lnum, col, off]
+
+		if mark_buf == bufnr and (uppercase_letter_mark(mark) or lowercase_letter_mark(mark)) then
+			M.add_mark_sign(mark, bufnr, mark_line)
+		end
+	end
+end
+
+local group = api.nvim_create_augroup(sign_group_name, { clear = true })
+
+-- This difference between `BufEnter` and `BufWinEnter`
+-- 1. BufEnter X:
+-- It was bufnr() != X
+-- But now it becomes bufnr() == X
+-- 2. BufWinEnter X:
+-- It was len(win_findbuf(X)) == 0
+-- But now it becomes len(win_findbuf(X)) == 1
+
+M.setup = function()
+	add_global_maps()
+
+	api.nvim_create_autocmd({ "BufEnter" }, {
+		group = group,
+		callback = function(args)
+			local bufnr = args.buf
+			-- local bt = api.nvim_get_option_value("buftype", { buf = bufnr })
+			-- if bt ~= "" then
+			-- 	return true
+			-- end
+
+			if not vim.b.mark_sign_init then
+				vim.b.mark_sign_init = true
+				add_buf_maps(bufnr)
+
+				-- The global marks
+				register_marks(fn.getmarklist(), bufnr)
+				-- The local marks
+				register_marks(fn.getmarklist(bufnr), bufnr)
+			end
+		end,
 	})
 end
 
-local sign_augroup = vim.api.nvim_create_augroup(sign_group_name, { clear = true })
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = sign_augroup,
-	callback = function(args)
-		local bufnr = args.buf
-		if cache_marks[bufnr] then
-			-- Remove all marks that were deleted.
-			for mark, _ in pairs(cache_marks[bufnr]) do
-				if vim.api.nvim_buf_get_mark(bufnr, mark)[1] == 0 then
-					delete_mark(mark, bufnr)
-					vim.print("delete mark '" .. mark)
-				end
-			end
-		end
-	end,
-})
-
--- Set up autocommands to refresh the signs.
-vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = sign_augroup,
-	callback = function(args)
-		local bufnr = args.buf
-
-		-- Only handle normal buffers.
-		local bt = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
-		if bt ~= "" then
-			return true
-		end
-
-		-- Init mark information for per buffer.
-		if not cache_marks[bufnr] then
-			cache_marks[bufnr] = {}
-		end
-
-		local local_marks = vim.fn.getmarklist(bufnr)
-		local global_marks = vim.fn.getmarklist()
-
-		local register_marks = function(marks)
-			for _, data in ipairs(marks) do
-				local mark = data.mark:sub(2, 3) -- name of the mark prefixed by "'"
-				local mark_buf, mark_line = unpack(data.pos) -- [bufnum, lnum, col, off]
-
-				local cached_mark = cache_marks[bufnr][mark]
-				if
-					mark_buf == bufnr
-					and is_letter_mark(mark)
-					and (not cached_mark or cached_mark.line ~= mark_line)
-				then
-					register_mark(mark, bufnr, mark_line)
-				end
-			end
-		end
-		-- The global marks
-		register_marks(vim.fn.getmarklist())
-		-- The local marks
-		register_marks(vim.fn.getmarklist(bufnr))
-
-		-- Set custom mappings.
-		on_attach(bufnr)
-	end,
-})
+M.setup()
