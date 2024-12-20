4fc8e92179591ac74d221e7f5e925d93498dd3ea
gh-liu <liugh.cs@gmail.com>
2024-12-20 17:13:29 +0800

    Update zsh-completions

--- script/cmp.sh
+++ script/cmp.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-podman completion zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_podman
-rustup completions zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_rustup
-gh completion -s zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_gh
-just --completions=zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_just
-git-absorb --gen-completions zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_git-absorb
-docker completion zsh >$XDG_CONFIG_HOME/zsh/zsh-completions/_docker
-SHELL=zsh bun completions >$XDG_CONFIG_HOME/zsh/zsh-completions/_bun
+[ -f "$(which rustup)" ] && rustup completions zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_rustup
+[ -f "$(which gh)" ] && gh completion -s zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_gh
+[ -f "$(which just)" ] && just --completions=zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_just
+[ -f "$(which git-absorb)" ] && git-absorb --gen-completions zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_git-absorb
+[ -f "$(which docker)" ] && docker completion zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_docker
+[ -f "$(which podman)" ] && podman completion zsh >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_podman
+[ -f "$(which bun)" ] && SHELL=zsh bun completions >"$XDG_CONFIG_HOME"/zsh/zsh-completions/_bun
