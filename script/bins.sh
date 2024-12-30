46d43ecc889180591e871abb1997fe236f2de979
gh-liu <liugh.cs@gmail.com>
2024-12-30 20:22:08 +0800

    Remeve bufls

--- script/bins.sh
+++ script/bins.sh
@@ -81,7 +81,6 @@ bins() {
 		go install github.com/google/yamlfmt/cmd/yamlfmt@latest
 		go install mvdan.cc/sh/v3/cmd/shfmt@latest
 
-		go install github.com/bufbuild/buf-language-server/cmd/bufls@latest
 		go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 		go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
