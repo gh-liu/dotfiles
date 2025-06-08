eeb445dd5be575102f054e096005e58747ecd91b
liugh <liugh.cs@gmail.com>
2025-06-09 06:45:46 +0800

    Add extended hugo

--- script/bins.sh
+++ script/bins.sh
@@ -91,7 +91,8 @@ bins() {
 		go install github.com/jesseduffield/lazygit@latest
 		go install github.com/jesseduffield/lazydocker@latest
 
-		go install github.com/gohugoio/hugo@latest
+		# go install github.com/gohugoio/hugo@latest
+		CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo@latest
 		go install golang.org/x/tools/cmd/present@latest
 
 		go install -ldflags "-s -w" github.com/tristanisham/zvm@latest
