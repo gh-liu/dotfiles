05018b6ef365241802dbd567edeaf9a87d829b8f
gh-liu <liugh.cs@gmail.com>
2025-06-18 10:49:20 +0800

    nvim: unmap `in/an`

--- xdg_config/nvim/lua/liu/config/keymaps.lua
+++ xdg_config/nvim/lua/liu/config/keymaps.lua
@@ -242,4 +242,19 @@ local function ctrl_g()
 end
 
 vim.keymap.set("n", "<c-g>", ctrl_g)
+
+local unmap = function(modes, lhs)
+	local modes_str = modes
+	if type(modes) == "table" then
+		modes_str = vim.iter(modes):join("")
+	end
+	if vim.fn.maparg(lhs, modes_str) ~= "" then
+		vim.keymap.del(modes, lhs)
+	end
+end
+-- disable `an/in` for lsp selectionRange
+-- https://github.com/neovim/neovim/pull/34011#issue-3061662405
+unmap("x", "in")
+unmap({ "x" }, "an")
+
 -- vim: foldmethod=marker
