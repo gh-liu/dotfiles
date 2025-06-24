520bacee150b221f7baac839b583a5806f22547a
liugh <liugh.cs@gmail.com>
2025-06-24 11:30:08 +0800

    nvim(lsp): add buf_ls

--- xdg_config/nvim/lua/liu/plugins/lsp.lua
+++ xdg_config/nvim/lua/liu/plugins/lsp.lua
@@ -233,6 +233,9 @@ return {
 				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vimls
 				-- @need-install: bun i -g vim-language-server
 				vimls = {},
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#buf_ls
+				-- @need-install: go install github.com/bufbuild/buf/cmd/buf@latest
+				buf_ls = {},
 			}
 
 			local other_caps = {}
