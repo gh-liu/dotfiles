5b43bea3c3cf094afdd150ed659ecd5e2e6e313c
gh-liu <liugh.cs@gmail.com>
2025-06-25 13:23:16 +0800

    nvim(lsp): add taplo for toml

--- xdg_config/nvim/lua/liu/plugins/lsp.lua
+++ xdg_config/nvim/lua/liu/plugins/lsp.lua
@@ -221,6 +221,27 @@ return {
 						-- require("liu.lsp.servers.jsonls").on_attach(client, bufnr)
 					end,
 				},
+				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#taplo
+				-- @need-install: cargo install --features lsp --locked taplo-cli
+				taplo = {
+					settings = {
+						-- Use the defaults that the VSCode extension uses:
+						-- https://github.com/tamasfe/taplo/blob/2e01e8cca235aae3d3f6d4415c06fd52e1523934/editors/vscode/package.json
+						taplo = {
+							configFile = { enabled = true },
+							schema = {
+								enabled = true,
+								catalogs = { "https://www.schemastore.org/api/json/catalog.json" },
+								cache = {
+									memoryExpiration = 60,
+									diskExpiration = 600,
+								},
+								-- Additional document and schema associations
+								associations = {},
+							},
+						},
+					},
+				},
 				-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#bashls
 				-- @need-install: bun i -g bash-language-server
 				bashls = {},
