f46e51ddebbb6e0f956a70f2a3fc98ce702cfca0
gh-liu <liugh.cs@gmail.com>
2025-07-01 21:30:18 +0800

    nvim(python): abbr

--- xdg_config/nvim/after/ftplugin/python.lua
+++ xdg_config/nvim/after/ftplugin/python.lua
@@ -2,3 +2,9 @@ vim.api.nvim_buf_create_user_command(0, "PyTest", function(opts)
 	-- TODO
 	print("https://docs.pytest.org/en/stable/explanation/goodpractices.html#test-discovery")
 end, { desc = "Py: generate or jump to test" })
+
+vim.cmd.inoreabbrev("<buffer> true True")
+vim.cmd.inoreabbrev("<buffer> false False")
+vim.cmd.inoreabbrev("<buffer> null None")
+vim.cmd.inoreabbrev("<buffer> none None")
+vim.cmd.inoreabbrev("<buffer> nil None")
