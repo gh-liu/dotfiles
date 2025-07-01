4168ffab6a9250d63de82e995bf13e7a4b5a04b2
gh-liu <liugh.cs@gmail.com>
2025-07-01 10:19:10 +0800

    ubuntu: format

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -28,13 +28,13 @@ sudo apt install -y gcc g++
 sudo apt install -y clangd
 sudo apt install -y gdb lldb-14
 
-sudo apt install -y  bpfcc-tools linux-headers-$(uname -r)
+sudo apt install -y bpfcc-tools linux-headers-$(uname -r)
 # dtrace
-sudo apt-get install systemtap-sdt-dev 
+sudo apt-get install systemtap-sdt-dev
 
 # build
 sudo apt install -y make cmake ninja-build
-sudo apt install -y graphviz                                                                                                                                                              allen in
+sudo apt install -y graphviz
 
 # databases
 sudo apt install -y sqlite3
