d88438b751fb025a8d47be9eec9b8fab55fc9e38
gh-liu <liugh.cs@gmail.com>
2025-07-25 17:57:15 +0800

    nvim: co- for git checkout -

--- xdg_config/nvim/lua/liu/plugins/git.lua
+++ xdg_config/nvim/lua/liu/plugins/git.lua
@@ -49,6 +49,7 @@ return { -- Git {{{2
 				callback = function(args)
 					vim.cmd([[
 					nnoremap <buffer> rt :<C-U>Git reset @~<C-R>=v:count1<CR><CR>
+					nnoremap <buffer> co- :<C-U>Git checkout -<CR>
 					]])
 				end,
 			})
