299fc834e2eaf69acd673945537347606690a2f7
gh-liu <liugh.cs@gmail.com>
2025-07-25 17:10:49 +0800

    nvim: qf diff use fugitive#DiffClose

--- xdg_config/nvim/after/plugin/fugitive_qf_diff.lua
+++ xdg_config/nvim/after/plugin/fugitive_qf_diff.lua
@@ -96,10 +96,11 @@ vim.api.nvim_create_autocmd({ "QuickFixCmdPost" }, {
 			callback = function(args)
 				local buf = args.buf
 				if bufs[buf] then
-					local diff_buf = vim.b[buf].diff_buf
-					if diff_buf and vim.api.nvim_buf_is_valid(diff_buf) then
-						vim.api.nvim_buf_delete(diff_buf, { force = true })
-					end
+					vim.fn["fugitive#DiffClose"]()
+					-- local diff_buf = vim.b[buf].diff_buf
+					-- if diff_buf and vim.api.nvim_buf_is_valid(diff_buf) then
+					-- 	vim.api.nvim_buf_delete(diff_buf, { force = true })
+					-- end
 				end
 			end,
 		})
