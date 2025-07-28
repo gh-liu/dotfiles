6dc1bd98701d0eb438c1debdffaade9a221a3be7
gh-liu <liugh.cs@gmail.com>
2025-07-28 14:14:25 +0800

    nvim: use main nvim-treesitter

--- xdg_config/nvim/lua/liu/plugins/treesitter.lua
+++ xdg_config/nvim/lua/liu/plugins/treesitter.lua
@@ -1,33 +1,34 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = "all",
-			ignore_install = {},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = function(lang, buf)
-					if vim.tbl_contains({ "tmux" }, lang) then
-						return true
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(event)
+					local filetype = event.match
+					local lang = vim.treesitter.language.get_lang(filetype)
+					if not lang then
+						return
 					end
 
-					local max_filesize = 256 * 1024 -- 256 KB
-					---@diagnostic disable-next-line: undefined-field
-					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
+					if not vim.tbl_contains(require("nvim-treesitter").get_available(), lang) then
+						return
+					end
+
+					vim.treesitter.start()
+
+					if vim.treesitter.query.get(lang, "folds") then
+						vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					end
+					if vim.treesitter.query.get(lang, "indents") then
+						vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
 					end
 				end,
-			},
-			indent = { enable = true },
-			incremental_selection = { enable = false },
-		},
-		config = function(self, opts)
-			require("nvim-treesitter.configs").setup(opts)
+				group = vim.api.nvim_create_augroup("liu/ts_setup", {}),
+			})
 		end,
+		branch = "main",
+		event = "VeryLazy",
+		build = ":TSUpdate",
+		opts = {},
 	},
 }
