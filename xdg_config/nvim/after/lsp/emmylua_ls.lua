7189dc31100023831c33dde77f642f7e1ca1427d
gh-liu <liugh.cs@gmail.com>
2025-08-01 13:28:42 +0800

    nvim: emmylua_ls config update

--- xdg_config/nvim/after/lsp/emmylua_ls.lua
+++ xdg_config/nvim/after/lsp/emmylua_ls.lua
@@ -1,3 +1,15 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#emmylua_ls
 -- @need-install: cargo install emmylua_ls
-return {}
+return {
+	-- https://github.com/EmmyLuaLs/emmylua-analyzer-rust/blob/main/docs/config/emmyrc_json_EN.md#-complete-configuration-example
+	settings = {
+		Lua = {
+			workspace = {
+				library = {
+					"$VIMRUNTIME/lua",
+					"$XDG_CONFIG_HOME/nvim/lua",
+				},
+			},
+		},
+	},
+}
