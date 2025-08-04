6c8cd00c1d31a9bf24605b0371b31e696d5ae45f
gh-liu <liugh.cs@gmail.com>
2025-08-04 09:32:13 +0800

    nvim(lsp): add docker_language_server

---
+++ xdg_config/nvim/after/lsp/docker_language_server.lua
@@ -0,0 +1,3 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#docker_language_server
+-- @need-install: go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
+return {}
