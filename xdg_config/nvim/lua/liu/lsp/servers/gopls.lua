4ebc75b9426e36e6b38765cfee2fc2ed6bdf791e
gh-liu <liugh.cs@gmail.com>
2025-08-14 13:26:27 +0800

    nvim: gopls impl use fzf-lua

--- xdg_config/nvim/lua/liu/lsp/servers/gopls.lua
+++ xdg_config/nvim/lua/liu/lsp/servers/gopls.lua
@@ -63,59 +63,106 @@ Impl.impl = function()
 
 	local struct_name = vim.treesitter.get_node_text(tsnode, 0)
 
-	local buf = api.nvim_get_current_buf()
+	local bufnr = api.nvim_get_current_buf()
 
-	Snacks.picker({
-		title = "Go Impl",
-		finder = function(opts, ctx)
-			local kind_interface = vim.lsp.protocol.SymbolKind.Interface
-			local method = vim.lsp.protocol.Methods.workspace_symbol
-			local param = {
-				query = ctx.filter.search,
+	local utils = require("fzf-lua.utils")
+	require("fzf-lua").fzf_live(function(args)
+		local query = args[1] or ""
+		local kind_interface = vim.lsp.protocol.SymbolKind.Interface
+		local method = vim.lsp.protocol.Methods.workspace_symbol
+		return function(cb)
+			local params = {
+				query = query,
 				kind = kind_interface,
 			}
-			local snacks_lsp = require("snacks.picker.source.lsp")
-			return function(cb)
-				snacks_lsp.request(buf, method, function()
-					return param
-				end, function(client, result, params)
-					local items = snacks_lsp.results_to_items(client, result, {
-						default_uri = params.textDocument and params.textDocument.uri or nil,
-						filter = function(item)
-							return item.kind == kind_interface
-						end,
-					})
-					for _, item in ipairs(items) do
-						---@diagnostic disable-next-line: await-in-sync
-						cb(item)
-					end
-				end)
+			local client = vim.lsp.get_clients({ name = "gopls", bufnr = bufnr })[1]
+			if not client then
+				cb(nil)
 			end
-		end,
-		live = true,
-		format = "lsp_symbol",
+			local handler = function(err, result, ctx)
+				result = vim.lsp.util.symbols_to_items(result, 0, client.offset_encoding)
+				result = vim.iter(result)
+					:filter(function(item)
+						return item.kind == "Interface"
+					end)
+					:each(function(item)
+						cb(item.text)
+					end)
+				cb(nil)
+			end
+			client:request(method, params, handler, bufnr)
+		end
+	end, {
+		prompt = "Go Impl> ",
 		actions = {
-			go_impl = function(self, item, action)
-				local symbol_name = item.item.name
+			default = function(selected, opts)
+				local select1 = selected[1]
+				local interfaceName, containerName = select1:match("%[Interface%]%s+(.+)%s+in%s+(.+)")
+				-- vim.print(interfaceName, containerName)
+
+				local symbol_name = interfaceName
 				local symbol_names = vim.split(symbol_name, "%.")
 				local interface_name = symbol_names[#symbol_names]
-				local package_name = item.item.containerName
-				self:close()
-				-- vim.print(symbol_name, package_name)
+				local package_name = containerName
 				vim.schedule(function()
 					local lines = Impl.gen_text(struct_name, package_name, interface_name)
 					Impl.append_text(tsnode:parent():parent(), lines)
 				end)
 			end,
 		},
-		win = {
-			input = {
-				keys = {
-					["<cr>"] = { "go_impl", mode = { "n", "i" } },
-				},
-			},
-		},
 	})
+
+	-- Snacks.picker({
+	-- 	title = "Go Impl",
+	-- 	finder = function(opts, ctx)
+	-- 		local kind_interface = vim.lsp.protocol.SymbolKind.Interface
+	-- 		local method = vim.lsp.protocol.Methods.workspace_symbol
+	-- 		local param = {
+	-- 			query = ctx.filter.search,
+	-- 			kind = kind_interface,
+	-- 		}
+	-- 		local snacks_lsp = require("snacks.picker.source.lsp")
+	-- 		return function(cb)
+	-- 			snacks_lsp.request(buf, method, function()
+	-- 				return param
+	-- 			end, function(client, result, params)
+	-- 				local items = snacks_lsp.results_to_items(client, result, {
+	-- 					default_uri = params.textDocument and params.textDocument.uri or nil,
+	-- 					filter = function(item)
+	-- 						return item.kind == kind_interface
+	-- 					end,
+	-- 				})
+	-- 				for _, item in ipairs(items) do
+	-- 					---@diagnostic disable-next-line: await-in-sync
+	-- 					cb(item)
+	-- 				end
+	-- 			end)
+	-- 		end
+	-- 	end,
+	-- 	live = true,
+	-- 	format = "lsp_symbol",
+	-- 	actions = {
+	-- 		go_impl = function(self, item, action)
+	-- 			local symbol_name = item.item.name
+	-- 			local symbol_names = vim.split(symbol_name, "%.")
+	-- 			local interface_name = symbol_names[#symbol_names]
+	-- 			local package_name = item.item.containerName
+	-- 			self:close()
+	-- 			-- vim.print(symbol_name, package_name)
+	-- 			vim.schedule(function()
+	-- 				local lines = Impl.gen_text(struct_name, package_name, interface_name)
+	-- 				Impl.append_text(tsnode:parent():parent(), lines)
+	-- 			end)
+	-- 		end,
+	-- 	},
+	-- 	win = {
+	-- 		input = {
+	-- 			keys = {
+	-- 				["<cr>"] = { "go_impl", mode = { "n", "i" } },
+	-- 			},
+	-- 		},
+	-- 	},
+	-- })
 end
 
 local M = {}
