064f58e6e2cd15ff8c79eda227a056de82f0ae5f
gh-liu <liugh.cs@gmail.com>
2025-08-18 20:06:37 +0800

    nvim: no snack, so no snack picker

--- xdg_config/nvim/.nvim.lua
+++ xdg_config/nvim/.nvim.lua
@@ -16,22 +16,3 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		vim.wo[0][0].foldexpr = "getline(v:lnum+1)=~'^\t\\{2}\"[a-zA-Z0-9-]\\+/[a-zA-Z0-9-]\\+'?'>1':'='"
 	end,
 })
-
-vim.keymap.set("n", "<leader>sP", function()
-	---@type snacks.picker.grep.Config
-	Snacks.picker({
-		finder = "grep",
-		format = "text",
-		search = function(picker)
-			return '^\t\t".*/.*",'
-		end,
-		transform = function(item)
-			local file = item.file
-			if file:match("plugins/(.*)%.lua") then
-				item.text = vim.trim(item.line)
-				return true
-			end
-			return false
-		end,
-	})
-end, {})
