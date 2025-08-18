d1486d829be2ed61d5d9c9eb356b166108bc3e7e
gh-liu <liugh.cs@gmail.com>
2025-08-18 19:58:03 +0800

    nvim: no server config for nvim-lspconfig

--- xdg_config/nvim/lua/liu/plugins/lsp.lua
+++ xdg_config/nvim/lua/liu/plugins/lsp.lua
@@ -14,30 +14,8 @@ return {
 		"neovim/nvim-lspconfig",
 		-- event = "VeryLazy",
 		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"saghen/blink.cmp", -- NOTE: capabilities
-		},
 		config = function(self, opts)
 			require("lspconfig.ui.windows").default_options.border = config.borders
-
-			local servers = {}
-
-			local other_caps = {}
-			local ok, blink_cmp = pcall(require, "blink.cmp")
-			if ok then
-				other_caps = blink_cmp.get_lsp_capabilities()
-			end
-
-			local capabilities = vim.tbl_deep_extend("force", vim.lsp.protocol.make_client_capabilities(), other_caps)
-			for server_name, server_config in pairs(servers) do
-				local default = {
-					-- on_attach = on_attach,
-					capabilities = capabilities,
-					settings = {},
-				}
-
-				require("lspconfig")[server_name].setup(vim.tbl_deep_extend("force", default, server_config))
-			end
 
 			set_hls({
 				LspInfoList = { link = "Function" },
