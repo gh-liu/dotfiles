2cb92c5d00dff5002f31308d967296387dddc39b
gh-liu <liugh.cs@gmail.com>
2025-08-26 17:35:29 +0800

    nvim: ast-grep lsp disable diagnostic

--- xdg_config/nvim/after/lsp/ast_grep.lua
+++ xdg_config/nvim/after/lsp/ast_grep.lua
@@ -1,3 +1,8 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ast_grep
 -- @need-install: cargo install ast-grep
-return {}
+return {
+	on_attach = function(client, buf)
+		local ns = vim.lsp.diagnostic.get_namespace(client.id)
+		vim.diagnostic.enable(false, { ns_id = ns })
+	end,
+}
