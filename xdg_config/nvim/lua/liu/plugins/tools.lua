1079d78d330c8069a055bdd6f5faa3c516e34744
gh-liu <liugh.cs@gmail.com>
2025-08-28 18:30:56 +0800

    nvim: deepseek avante.nvim

--- xdg_config/nvim/lua/liu/plugins/tools.lua
+++ xdg_config/nvim/lua/liu/plugins/tools.lua
@@ -108,7 +108,17 @@ return {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
 		},
-		opts = {},
+		opts = {
+			provider = "deepseek",
+			providers = {
+				deepseek = {
+					__inherited_from = "openai",
+					api_key_name = "DEEPSEEK_API_KEY",
+					endpoint = "https://api.deepseek.com",
+					model = "deepseek-coder",
+				},
+			},
+		},
 	},
 	{
 		"tpope/vim-dadbod",
