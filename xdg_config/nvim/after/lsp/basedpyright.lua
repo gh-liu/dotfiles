409fe3f10c8c7ab0fd30dbb9cd31f5357d25d07a
gh-liu <liugh.cs@gmail.com>
2025-08-28 16:17:13 +0800

    nvim: config basedpyright

--- xdg_config/nvim/after/lsp/basedpyright.lua
+++ xdg_config/nvim/after/lsp/basedpyright.lua
@@ -1,3 +1,18 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#basedpyright
 -- @need-install: uv tool install --force basedpyright
-return {}
+---@type vim.lsp.Config
+local Config = {
+	settings = {
+		basedpyright = {
+			analysis = {
+				-- https://docs.basedpyright.com/latest/configuration/config-files/#type-check-diagnostics-settings
+				---@alias SeverityOverridesValue 'error'|'warning'|'information'|'true'|'false'|'none'
+				---@type table<string,SeverityOverridesValue>
+				diagnosticSeverityOverrides = {
+					reportAny = false,
+				},
+			},
+		},
+	},
+}
+return Config
