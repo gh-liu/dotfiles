6b27c941f81127d18c4c737d012e995324b7909e
gh-liu <liugh.cs@gmail.com>
2025-08-29 16:26:45 +0800

    nvim(lsp): ok, still ast_grep

--- xdg_config/nvim/after/lsp/ast_grep.lua
+++ xdg_config/nvim/after/lsp/ast_grep.lua
@@ -1,3 +1,28 @@
+vim.api.nvim_create_user_command("AstGrep", function(cmd_args)
+	if cmd_args.fargs[1] == "playground" then
+		vim.ui.open("https://ast-grep.github.io/playground.html")
+		return
+	end
+
+	if cmd_args.fargs[1] == "diagnostics" then
+		local client = vim.lsp.get_clients({ name = "ast_grep" })[1]
+		if not client then
+			return
+		end
+		local ns = vim.lsp.diagnostic.get_namespace(client.id)
+		vim.diagnostic.setqflist({ namespace = ns, open = true })
+		return
+	end
+end, {
+	nargs = "?",
+	complete = function()
+		return {
+			"playground",
+			"diagnostics",
+		}
+	end,
+})
+
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ast_grep
 -- @need-install: cargo install ast-grep
 return {
