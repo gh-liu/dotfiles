8bda0c43fde9d592493fd284c1aa373485f49eb4
gh-liu <liugh.cs@gmail.com>
2025-08-30 11:29:26 +0800

    nvim: remove s3

--- xdg_config/nvim/after/plugin/s3.lua
+++
@@ -1,8 +0,0 @@
--- https://czak.pl/posts/s3-rest-api-with-curl
--- https://gist.github.com/Mindgames/556dc7d1e452d0cefcb7
--- https://gist.github.com/tuxfight3r/7ccbd5abc4ded37ecdbc8fa46966b7e8
-
--- autocmd BufReadCmd   s3://*
--- autocmd BufWriteCmd  s3://*
--- autocmd FileReadCmd  s3://*
--- autocmd FileWriteCmd s3://*
