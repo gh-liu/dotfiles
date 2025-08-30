96c86e93ae554f4e3e9d4e4d9e68474e5fe8276f
gh-liu <liugh.cs@gmail.com>
2025-08-30 08:45:06 +0800

    nvim: plugin cd

--- xdg_config/nvim/lua/liu/config/keymaps.lua
+++ xdg_config/nvim/lua/liu/config/keymaps.lua
@@ -111,32 +111,6 @@ nnoremap y<leader> :execute 'let @+ = substitute(expand("%:p"), getcwd()."/","",
 "noremap - :<C-U>cd .. <CR>
 ]])
 
--- vim.keymap.set("n", "0cd", function()
--- 	local ok, _ = pcall(vim.cmd, "lcd -")
--- 	if not ok then
--- 		local gcwd = vim.fn.getcwd(-1, 0)
--- 		vim.cmd.lcd(gcwd)
--- 	end
--- end)
-vim.keymap.set("n", "cd", function()
-	local bufname = vim.api.nvim_buf_get_name(0)
-	if bufname == "" or vim.fn.filereadable(bufname) == 0 then
-		bufname = vim.fn.getcwd()
-	end
-	local dirs = {}
-	for dir in vim.fs.parents(bufname) do
-		table.insert(dirs, dir)
-	end
-	-- dirs = vim.fn.reverse(dirs)
-	local dir = dirs[vim.v.count1]
-	if dir then
-		vim.cmd.lcd(dirs[vim.v.count1])
-	else
-		local gcwd = vim.fn.getcwd(-1, 0)
-		vim.cmd.lcd(gcwd)
-	end
-end)
-
 -- Tools
 vim.cmd([[
 " print unix time at cursor as human-readable datetime. 1677604904 => '2023-02-28 09:21:45'
