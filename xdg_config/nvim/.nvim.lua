6722c5056a7ac6c641efb924ebd109cfe262b985
gh-liu <liugh.cs@gmail.com>
2025-09-04 13:47:08 +0800

    nvim: LazyPlugins

--- xdg_config/nvim/.nvim.lua
+++ xdg_config/nvim/.nvim.lua
@@ -16,3 +16,44 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		vim.wo[0][0].foldexpr = "getline(v:lnum+1)=~'^\t\\{2}\"[a-zA-Z0-9-]\\+/[a-zA-Z0-9-]\\+'?'>1':'='"
 	end,
 })
+
+vim.api.nvim_create_user_command("LazyPlugins", function()
+	local obj = vim.system({ "rg", '^\t{2}".*/.*",', vim.fn.stdpath("config") .. "/lua/liu/plugins", "--json" }):wait()
+	local lines = vim.split(obj.stdout, "\n", { trimempty = true })
+	local result = vim.iter(lines)
+		:map(function(line)
+			return vim.json.decode(line)
+		end)
+		:filter(function(res)
+			return res["type"] and res["type"] == "match"
+		end)
+		:map(function(res)
+			local data = res.data
+			return {
+				path = data.path.text,
+				line = data.line_number,
+				text = vim.trim(data.lines.text),
+			}
+		end)
+		:totable()
+
+	table.sort(result, function(a, b)
+		return a.text < b.text
+	end)
+
+	vim.ui.select(result, {
+		format_item = function(item)
+			return item.text
+		end,
+	}, function(choice)
+		if not choice then
+			return
+		end
+		local match = vim.iter(result):find(function(item)
+			return item.text == choice.text
+		end)
+		if match then
+			vim.cmd("e" .. " +" .. match.line .. " " .. match.path)
+		end
+	end)
+end, {})
