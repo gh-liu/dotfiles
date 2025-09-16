7b47fc092b5082ff48b4559f5f01b1b437961abb
gh-liu <liugh.cs@gmail.com>
2025-09-16 20:01:14 +0800

    nvim(lsp): osc 9;4

--- xdg_config/nvim/lua/liu/lsp/init.lua
+++ xdg_config/nvim/lua/liu/lsp/init.lua
@@ -300,6 +300,19 @@ handlers[ms.textDocument_rename] = function(...)
 end
 -- }}}
 
+vim.api.nvim_create_autocmd("LspProgress", {
+	callback = function(ev)
+		local value = ev.data.params.value
+		if value.kind == "begin" then
+			vim.api.nvim_ui_send("\027]9;4;1;0\027\\")
+		elseif value.kind == "end" then
+			vim.api.nvim_ui_send("\027]9;4;0\027\\")
+		elseif value.kind == "report" then
+			vim.api.nvim_ui_send(string.format("\027]9;4;1;%d\027\\", value.percentage or 0))
+		end
+	end,
+})
+
 -- lsp tools {{{1
 local on_attachs = {}
 local tool_dir = "liu/lsp/tools/"
