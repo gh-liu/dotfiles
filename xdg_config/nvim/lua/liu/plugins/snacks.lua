38708509a34623c1b46b84a1cf607e045787a42f
gh-liu <liugh.cs@gmail.com>
2025-10-24 17:49:49 +0800

    nvim: snacks

---
+++ xdg_config/nvim/lua/liu/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		picker = { enabled = true },
+		-- ===========================
+		bigfile = { enabled = false },
+		dashboard = { enabled = false },
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = false },
+		notifier = { enabled = false },
+		quickfile = { enabled = false },
+		scope = { enabled = false },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+	},
+}
