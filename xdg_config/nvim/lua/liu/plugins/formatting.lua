8f768801dd46c4ccc3400e534434442e3ebf52fa
gh-liu <liugh.cs@gmail.com>
2025-11-05 14:04:58 +0800

    nvim: no need Format command

--- xdg_config/nvim/lua/liu/plugins/formatting.lua
+++ xdg_config/nvim/lua/liu/plugins/formatting.lua
@@ -71,7 +71,6 @@ return {
 			end,
 			desc = "Toggle autoformat",
 		},
-		{ "gq<CR>", "<cmd>Format<CR>" },
 	},
 	ft = function(self, ft)
 		return vim.tbl_keys(self.opts.formatters_by_ft)
@@ -94,18 +93,6 @@ return {
 			}
 		end
 		require("conform").setup(opts)
-
-		vim.api.nvim_create_user_command("Format", function(args)
-			local range = nil
-			if args.count ~= -1 then
-				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-				range = {
-					start = { args.line1, 0 },
-					["end"] = { args.line2, end_line:len() },
-				}
-			end
-			require("conform").format({ async = true, lsp_format = "fallback", range = range })
-		end, { range = true })
 
 		require("conform").formatters.sqlfluff = function(bufnr)
 			local dialect = vim.b[bufnr].sql_dialect or vim.b.sql_type_override or vim.g.sql_type_default
