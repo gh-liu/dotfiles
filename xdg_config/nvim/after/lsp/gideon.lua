213b23e4cf6727e2d90293ccfcba29c27d06eb03
gh-liu <liugh.cs@gmail.com>
2025-11-07 09:55:47 +0800

    nvim: lsp gideon

--- xdg_config/nvim/after/lsp/gideon.lua
+++ xdg_config/nvim/after/lsp/gideon.lua
@@ -1,6 +1,5 @@
 ---@type vim.lsp.Config
 local Config = {
-	cmd = { "uv", "run", vim.fn.expand("~/dev/gideon/main.py") },
-	cmd_cwd = vim.fn.expand("~/dev/gideon"),
+	cmd = { "gideon" },
 }
 return Config
