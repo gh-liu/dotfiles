76f03b2992ed77db6b7d862d3993145ec2d1086c
gh-liu <liugh.cs@gmail.com>
2025-11-07 09:17:26 +0800

    nvim: lsp maps

--- xdg_config/nvim/lua/liu/lsp/init.lua
+++ xdg_config/nvim/lua/liu/lsp/init.lua
@@ -183,9 +183,9 @@ api.nvim_create_autocmd("LspAttach", {
 		-- end
 
 		-- NOTE: default lsp mapping prefix with `gr`
-		nmap("grL", lsp.codelens.run, "Code [L]en")
+		nmap("grl", lsp.codelens.run, "Code [L]en")
 
-		vim.keymap.set("i", "<C-]>", lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Documentation" })
+		-- vim.keymap.set("i", "<C-]>", lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Documentation" })
 	end,
 })
 -- }}}
