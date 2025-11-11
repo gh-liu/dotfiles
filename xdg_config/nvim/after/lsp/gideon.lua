c5de706e3962b49a43b28d18da79a54fe49dea07
gh-liu <liugh.cs@gmail.com>
2025-11-11 23:21:31 +0800

    nvim: lsp gideon

--- xdg_config/nvim/after/lsp/gideon.lua
+++ xdg_config/nvim/after/lsp/gideon.lua
@@ -1,5 +1,10 @@
 ---@type vim.lsp.Config
 local Config = {
 	cmd = { "gideon" },
+	settings = {
+		base_url = vim.env.OPENAI_BASE_URL,
+		token = vim.env.OPENAI_APIKEY,
+		model = vim.env.OPENAI_MODEL,
+	},
 }
 return Config
