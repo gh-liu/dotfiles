cec688058906f831bffa2893375bc8b039d2b927
gh-liu <liugh.cs@gmail.com>
2025-11-24 17:39:08 +0800

    nvim: lsp gideon

--- xdg_config/nvim/after/lsp/gideon.lua
+++ xdg_config/nvim/after/lsp/gideon.lua
@@ -4,12 +4,14 @@ local Config = {
 		baseUrl = vim.env.OPENAI_BASE_URL,
 		apiKey = vim.env.OPENAI_API_KEY,
 		model = vim.env.OPENAI_MODEL,
+		collName = "testColl",
 	},
 	cmd = { "gideon" },
 	settings = {
 		baseUrl = vim.env.OPENAI_BASE_URL,
 		apiKey = vim.env.OPENAI_API_KEY,
 		model = vim.env.OPENAI_MODEL,
+		collName = "testColl",
 	},
 	root_dir = vim.fs.root(0, { ".git" }),
 }
