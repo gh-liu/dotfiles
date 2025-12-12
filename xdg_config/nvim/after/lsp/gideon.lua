841971cb46e0e36919a5533b92ba3fdfd93cc588
gh-liu <liugh.cs@gmail.com>
2025-12-12 09:54:06 +0800

    nvim(gideon): use openrouter

--- xdg_config/nvim/after/lsp/gideon.lua
+++ xdg_config/nvim/after/lsp/gideon.lua
@@ -1,18 +1,21 @@
 ---@type vim.lsp.Config
 local Config = {
 	init_options = {
-		baseUrl = vim.env.OPENAI_BASE_URL,
-		apiKey = vim.env.OPENAI_API_KEY,
-		model = vim.env.OPENAI_MODEL,
+		-- baseUrl = vim.env.OPENAI_BASE_URL,
+		-- apiKey = vim.env.OPENAI_API_KEY,
+		-- model = vim.env.OPENAI_MODEL,
+		baseUrl = vim.env.OPENROUTER_BASE_URL,
+		apiKey = vim.env.OPENROUTER_API_KEY,
+		model = vim.env.OPENROUTER_MODEL,
 		collName = "testColl",
 	},
 	cmd = { "gideon" },
-	settings = {
-		baseUrl = vim.env.OPENAI_BASE_URL,
-		apiKey = vim.env.OPENAI_API_KEY,
-		model = vim.env.OPENAI_MODEL,
-		collName = "testColl",
-	},
+	-- settings = {
+	-- 	baseUrl = vim.env.OPENAI_BASE_URL,
+	-- 	apiKey = vim.env.OPENAI_API_KEY,
+	-- 	model = vim.env.OPENAI_MODEL,
+	-- 	collName = "testColl",
+	-- },
 	root_dir = vim.fs.root(0, { ".git" }),
 	filetypes = { "go" },
 }
