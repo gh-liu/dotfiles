cb5bad07292aaac6988c7f6a87b9a8beea8ee055
gh-liu <liugh.cs@gmail.com>
2025-12-19 10:53:01 +0800

    nvim: fix buf project root

--- xdg_config/nvim/after/plugin/buf_project_root.lua.lua
+++ xdg_config/nvim/after/plugin/buf_project_root.lua.lua
@@ -1,28 +1,51 @@
-local auto_root_dirs = {
-	".git",
-	-- "Makefile",
-	-- "go.mod", -- go
-	-- "Cargo.toml", -- rust
-	-- "build.zig.zon", -- zig
-	-- "pyproject.toml", -- python
-	".nvimrc", -- :h exrc
-	".nvim.lua", -- :h exrc
-	".projections.json", -- :h projectionist-setup
-}
-if vim.fn.has("nvim-0.11.3") == 1 then
-	auto_root_dirs = { auto_root_dirs }
+-- global -> tabpage -> window
+-- see chdir()
+local getcwds = function()
+	return {
+		window = vim.fn.getcwd(0, 0),
+		tabpage = vim.fn.getcwd(-1, 0),
+		global = vim.fn.getcwd(-1, -1),
+	}
 end
 
-vim.api.nvim_create_autocmd("BufEnter", {
+local auto_root_dirs = {
+	{
+		".git",
+		-- "Makefile",
+		-- "go.mod", -- go
+		-- "Cargo.toml", -- rust
+		-- "build.zig.zon", -- zig
+		-- "pyproject.toml", -- python
+		".nvimrc", -- :h exrc
+		".nvim.lua", -- :h exrc
+		".projections.json", -- :h projectionist-setup
+	}, -- make those dirs same priority
+}
+
+vim.o.autochdir = false
+vim.api.nvim_create_autocmd({
+	-- "SessionLoadPost",
+	"VimEnter",
+}, {
 	group = vim.api.nvim_create_augroup("setup_auto_root", {}),
-	callback = function(data)
-		-- vim.o.autochdir = false
-		local root = vim.fs.root(data.buf, auto_root_dirs)
-		if root == nil or root == vim.fn.getcwd() then
+	callback = function(_data)
+		if vim.fn.empty(vim.v.this_session) == 0 then
 			return
 		end
-		vim.fn.chdir(root)
+
+		local buf = vim.api.nvim_get_current_buf()
+		if not vim.api.nvim_buf_is_valid(buf) or vim.bo[buf].buftype ~= "" then
+			return
+		end
+
+		local root = vim.fs.root(buf, auto_root_dirs)
+		if root == nil or root == vim.fn.getcwd(-1, 0) then
+			return
+		end
+		-- vim.print(getcwds())
+		vim.fn.chdir(root, "tabpage")
 		vim.api.nvim_echo({ { "chdir to " .. root, "WarningMsg" } }, true, {})
+		-- vim.print(getcwds())
 	end,
 	desc = "Find root and change current directory",
 	once = true,
