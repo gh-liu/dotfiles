728cd757ed8ad43768b4c117b8c7a67cd2bf6674
gh-liu <liugh.cs@gmail.com>
2026-01-07 10:11:31 +0800

    nvim: delete nvim.bufa

--- xdg_config/nvim/lua/liu/plugins/editor.lua
+++ xdg_config/nvim/lua/liu/plugins/editor.lua
@@ -405,19 +405,6 @@ return {
 		end,
 	},
 	{
-		"mistweaverco/bafa.nvim",
-		keys = {
-			{ "gb", "<cmd>lua require('bafa.ui').toggle()<cr>" },
-		},
-		opts = {
-			ui = {
-				title = {
-					text = "bafa",
-				},
-			},
-		},
-	},
-	{
 		"MagicDuck/grug-far.nvim",
 		enabled = false,
 		opts = {
