42a6172644e7a1aa8d4af1830ae84eb79aa94f78
gh-liu <liugh.cs@gmail.com>
2026-01-16 13:12:51 +0800

    nvim: fix taplo lsp

--- xdg_config/nvim/after/lsp/taplo.lua
+++ xdg_config/nvim/after/lsp/taplo.lua
@@ -1,6 +1,5 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#taplo
--- @need-install: bun install -g @taplo/cli
--- cargo install --features lsp --locked taplo-cli
+-- @need-install: cargo install --features lsp --locked taplo-cli
 return {
 	settings = {
 		-- Use the defaults that the VSCode extension uses:
