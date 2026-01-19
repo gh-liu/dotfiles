c4083c9c6a85029c1d62f01fd1ed314b8861972c
gh-liu <liugh.cs@gmail.com>
2026-01-19 17:27:31 +0800

    nvim: FormatJsonText

--- xdg_config/nvim/after/plugin/jq.lua
+++ xdg_config/nvim/after/plugin/jq.lua
@@ -8,7 +8,4 @@ vim.api.nvim_create_user_command("ToJsonText", function(_)
 	vim.cmd("%!jq -c '. | tojson'")
 end, { nargs = 0 })
 
-vim.api.nvim_create_user_command("CompactJsonText", function(_)
-	vim.cmd("setlocal ft=json")
-	vim.cmd("%!jq -c .")
-end, { nargs = 0 })
+vim.cmd([[command! -bang FormatJsonText setlocal ft=json | execute '%!jq ' . (<bang>0 ? '-c ' : '') . '.']])
