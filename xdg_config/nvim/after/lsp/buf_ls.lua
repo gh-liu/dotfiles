22e675ffd86efc5218312198bddb0091fd757cd8
gh-liu <liugh.cs@gmail.com>
2026-03-06 09:49:58 +0800

    nvim: lsp buf_ls

--- xdg_config/nvim/after/lsp/buf_ls.lua
+++ xdg_config/nvim/after/lsp/buf_ls.lua
@@ -1,3 +1,4 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#buf_ls
 -- @need-install: go install github.com/bufbuild/buf/cmd/buf@latest
+---@type vim.lsp.Config
 return {}
