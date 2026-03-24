eff9f0a79c5f563830d59077b19ec5aa39488253
gh-liu <liugh.cs@gmail.com>
2026-03-24 10:13:34 +0800

    ubuntu

--- script/ubuntu.sh
+++ script/ubuntu.sh
@@ -3,84 +3,21 @@
 # PACKAGES
 sudo apt update && sudo apt upgrade -y
 
-# remote access
-sudo apt install -y \
-        openssh-server
-
-# shell environment
-sudo apt install -y \
-        zsh \
-        tmux tmuxp
-
-# editors and text search
+# cli: editors and text search
 sudo apt install -y \
         vim \
         fzf ripgrep
 
-# archives
+# cli: archives
 sudo apt install -y \
         zip unzip
 
-# download and transfer
-sudo apt install -y \
-        curl wget nghttp2
-
-# general cli utils
+# cli: general utils
 sudo apt install -y \
         jq \
         tldr direnv btop cloc
 
-# version control
-sudo apt install -y \
-        git git-flow
-
-# build essentials
-sudo apt install -y \
-        build-essential \
-        make cmake ninja-build \
-        binutils \
-        patchutils \
-        graphviz
-
-# c and cpp compilers
-sudo apt install -y \
-        gcc g++
-
-# c and cpp debugging and indexing
-sudo apt install -y \
-        clangd \
-        gdb lldb-14
-
-# development libraries
-sudo apt install -y \
-        libncurses5-dev libevent-dev
-
-# kernel headers
-sudo apt install -y \
-        linux-headers-$(uname -r)
-
-# performance tools
-sudo apt install -y \
-        linux-tools-common linux-tools-generic linux-tools-$(uname -r) \
-        sysstat \
-        bpfcc-tools
-
-# network tools
-sudo apt install -y \
-        net-tools bridge-utils
-
-# tracing
-sudo apt install -y \
-        systemtap-sdt-dev
-
-# containers
-sudo apt install -y podman \
-        bubblewrap
-
-# sudo apt install -y gh
-# sudo apt install -y kitty
-
-# database clients
+# db: clients
 sudo apt install -y \
         sqlite3 \
         redis-tools \
@@ -88,13 +25,73 @@ sudo apt install -y \
 # mongodb: https://www.mongodb.com/try/download/shell
 wget https://downloads.mongodb.com/compass/mongodb-mongosh_2.5.6_amd64.deb | sudo apt install ./mongodb-mongosh_2.5.6_amd64.deb
 
+# dev: version control
+sudo apt install -y \
+        git git-flow
+
+# dev: build essentials
+sudo apt install -y \
+        build-essential \
+        make cmake ninja-build \
+        binutils \
+        patchutils \
+        graphviz
+
+# dev: c and cpp compilers
+sudo apt install -y \
+        gcc g++
+
+# dev: debugging and indexing
+sudo apt install -y \
+        clangd \
+        gdb lldb-14
+
+# dev: development libraries
+sudo apt install -y \
+        libncurses5-dev libevent-dev
+
 # lang
 # sudo apt install -y python3-pip
 
-# nodejs runtime
+# lang: nodejs runtime
 # https://deb.nodesource.com/
 # curl -fsSL https://deb.nodesource.com/setup_22.x | sudo bash -
 sudo apt install -y nodejs
+
+# net: download and transfer
+sudo apt install -y \
+        curl wget nghttp2
+
+# net: diagnostics and bridging
+sudo apt install -y \
+        net-tools bridge-utils
+
+# sys: remote access
+sudo apt install -y \
+        openssh-server
+
+# sys: shell environment
+sudo apt install -y \
+        zsh \
+        tmux tmuxp
+
+# sys: kernel headers
+sudo apt install -y \
+        linux-headers-$(uname -r)
+
+# sys: performance tools
+sudo apt install -y \
+        linux-tools-common linux-tools-generic linux-tools-$(uname -r) \
+        sysstat \
+        bpfcc-tools
+
+# sys: tracing
+sudo apt install -y \
+        systemtap-sdt-dev
+
+# sys: containers and sandboxing
+sudo apt install -y podman \
+        bubblewrap
 
 # # For full system emulation
 # sudo apt install -y qemu-system
@@ -102,6 +99,9 @@ sudo apt install -y nodejs
 # sudo apt install -y qemu-user-static
 
 # sudo apt install -y proxychains4
+
+# sudo apt install -y gh
+# sudo apt install -y kitty
 
 # DIRECTORY
 mkdir -p $HOME/env/{golang,rust,zig,lua,nodejs}
