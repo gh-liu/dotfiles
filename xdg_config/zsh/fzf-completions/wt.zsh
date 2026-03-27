a7c2172b3a163d35cc3798a089e95394b819057d
gh-liu <liugh.cs@gmail.com>
2026-03-27 09:02:42 +0800

    zsh: fix fzf for wt

--- xdg_config/zsh/fzf-completions/wt.zsh
+++ xdg_config/zsh/fzf-completions/wt.zsh
@@ -2,8 +2,8 @@
 _fzf_complete_wt() {
 	local tokens=(${(z)LBUFFER})
 	local cmd=${tokens[2]-}
-	
+
 	if [[ "$cmd" == "switch" || "$cmd" == "remove" ]]; then
-		_fzf_complete -m -- "$@" < <(git worktree list 2>/dev/null | awk '{match($0, /\[(.+)\]/, arr); if (arr[1]) print arr[1]}')
+		_fzf_complete -- "$@" < <(git worktree list 2>/dev/null | awk '{match($0, /\[(.+)\]/, arr); if (arr[1]) print arr[1]}')
 	fi
 }
