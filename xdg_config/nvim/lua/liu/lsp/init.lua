f35e7af102b92c31db940ea551d483121e3276fd
gh-liu <liugh.cs@gmail.com>
2026-03-30 10:47:39 +0800

    nvim: fix lsp progress

--- xdg_config/nvim/lua/liu/lsp/init.lua
+++ xdg_config/nvim/lua/liu/lsp/init.lua
@@ -303,7 +303,8 @@ vim.api.nvim_create_autocmd("LspProgress", {
 	callback = function(ev)
 		local value = ev.data.params.value
 		vim.api.nvim_echo({ { value.message or "done" } }, false, {
-			id = "lsp",
+			id = ev.data.id,
+			source = "vim.lsp",
 			kind = "progress",
 			title = value.title,
 			status = value.kind ~= "end" and "running" or "success",
