b73d43e2e745ba5c87a58412ce880914f1bf78eb
gh-liu <liugh.cs@gmail.com>
2026-05-11 14:26:40 +0800

    nvim: tune ftplugin for python

--- xdg_config/nvim/after/ftplugin/python.lua
+++ xdg_config/nvim/after/ftplugin/python.lua
@@ -3,10 +3,3 @@ vim.cmd.inoreabbrev("<buffer> false False")
 vim.cmd.inoreabbrev("<buffer> null None")
 vim.cmd.inoreabbrev("<buffer> none None")
 vim.cmd.inoreabbrev("<buffer> nil None")
-
-local python_project_root = vim.fs.root(0, { "pyproject.toml", "uv.lock", "requirements.txt", ".venv" })
-vim.b.is_python_project = python_project_root ~= nil
-
-if not vim.b.is_python_project then
-	vim.b.dispatch = "uv run --script %"
-end
