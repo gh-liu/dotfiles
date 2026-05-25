02255c4d8056a51fbf67b139e07cc2267594c880
gh-liu <liugh.cs@gmail.com>
2026-05-25 10:31:58 +0800

    nvim: lsp buf_ls

--- xdg_config/nvim/after/lsp/buf_ls.lua
+++ xdg_config/nvim/after/lsp/buf_ls.lua
@@ -1,3 +1,12 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#buf_ls
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = vim.api.nvim_create_augroup("liu.lsp.buf_ls", { clear = true }),
+	pattern = { "buf.yaml", "buf.gen.yaml" },
+	callback = function()
+		vim.schedule(function()
+			vim.lsp.start(vim.lsp.config["buf_ls"])
+		end)
+	end,
+})
 ---@type vim.lsp.Config
 return {}
