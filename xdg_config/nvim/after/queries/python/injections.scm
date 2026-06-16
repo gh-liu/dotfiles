5df39841bb69a574d002e73871a0fe072b05db6d
gh-liu <liugh.cs@gmail.com>
2026-06-16 14:20:43 +0800

    nvim: python inject ft:*

--- xdg_config/nvim/after/queries/python/injections.scm
+++ xdg_config/nvim/after/queries/python/injections.scm
@@ -7,3 +7,24 @@
       (string_content) @injection.content)))
   (#any-of? @func_call "conn.execute")
   (#set! injection.language "sql"))
+
+; Inject language based on # ft:<lang> comment above a string
+; [] matches either bare string or assignment (e.g., data = """...""")
+; #gsub! strips the prefix, @injection.language reads the modified text
+(
+  (comment) @injection.language
+  .
+  (_
+    [
+      (string
+        (string_content) @injection.content)
+      (assignment
+        (string
+          (string_content) @injection.content))
+      (return_statement
+        (string
+          (string_content) @injection.content))
+    ])
+  (#match? @injection.language "^\\s*#\\s*ft:\\s*[a-zA-Z_]+")
+  (#gsub! @injection.language "^%s*#%s*ft:%s*" "")
+  (#gsub! @injection.language "%s*$" ""))
