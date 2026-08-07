68482dcfe4da65bb4cc4c2055712123833671b66
gh-liu <liugh.cs@gmail.com>
2026-08-07 22:12:45 +0800

    pi: remove config

--- xdg_config/pi/agent/extensions/config/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-
-export default function(pi: ExtensionAPI) {
-  pi.on("agent_end", async () => {
-    process.stdout.write("\x07");
-  });
-}
